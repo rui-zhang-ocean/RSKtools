3b703f374d62076b0bcfff08d327972468685585
Colin Jones <colin.jones@rbr-global.com>
2016-09-22 13:30:59 -0400

    Update to latest (1.13.0) RSK DB version

--- RSKconstants.m
+++ RSKconstants.m
@@ -5,10 +5,10 @@
 % Website: www.rbr-global.com
 % Last revision: 2016-05-16
 
-latestRSKversion = '1.12.2';
+latestRSKversion = '1.13.0';
 latestRSKversionMajor = 1;
-latestRSKversionMinor = 12;
-latestRSKversionPatch = 2;
+latestRSKversionMinor = 13;
+latestRSKversionPatch = 0;
 
 eventBeginUpcast = 33;
 eventBeginDowncast = 34;
