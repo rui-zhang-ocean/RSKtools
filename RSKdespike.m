fbaf30b31641c2adbc0a325998aed8b5fa1f24a4
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-11-04 15:58:34 -0400

    Update to use input parsing.
    RSKTOOLS-30

--- RSKdespike.m
+++ RSKdespike.m
@@ -1,8 +1,8 @@
-function y = RSKdespike(x, varargin)
+function [RSK] = RSKdespike(RSK, varargin)
 
 % RSKdespike - De-spike a time series using a running median filter.
 %
-% Syntax:  [y] = RSKdespike(x, [OPTIONS])
+% Syntax:  [RSK] = RSKdespike(RSK, [OPTIONS])
 % 
 % RSKdespike is a despike algorithm that utilizes a running median
 % filter to create a reference series. Each point in the original
@@ -13,9 +13,24 @@ function y = RSKdespike(x, varargin)
 %
 % Inputs:
 %    
-%   [Required] - x - the input time series
+%   [Required] - RSK - the input RSK structure, with profiles as read using
+%                    RSKreadprofiles
 %
-%   [Optional] - n - the number of standard deviations to use for the spike criterion.
+%   [Optional] - channel - Longname of channel to plot (e.g. temperature,
+%                   salinity, etc). Default is ;Temperature'
+%
+%                series - the data series to apply correction. Must be
+%                   either 'data' or 'profile'. If 'data' must run RSKreaddata() 
+% .                 before RSKdespike, if 'profile' must first run RSKreadprofiles().
+%                   Default is 'data'.
+%
+%                profileNum - the profiles to which to apply the correction. If
+%                    left as an empty vector, will do all profiles.
+%            
+%                direction - the profile direction to consider. Must be either
+%                   'down' or 'up'. Only needed if series is profile. Defaults to 'down'.
+%
+%                n - the number of standard deviations to use for the spike criterion.
 %                   Default value is 4.
 %
 %                k - the length of the running median. Default value is 7.
@@ -28,34 +43,88 @@ function y = RSKdespike(x, varargin)
 %    y - the de-spiked series
 %
 % Example: 
-%    temperatureDS = RSKdespike(rsk.data.values(:,2))
+%    temperatureDS = RSKdespike(RSK)
 %   OR
-%    temperatureDS = RSKdespike(rsk.data.values(:,2), 'n',2, 'k',10, 'action','NaN');
+%    temperatureDS = RSKdespike(RSK, 'n',2, 'k',10, 'action','NaN');
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-10-02
+% Last revision: 2016-11-04
 
-%Set input and default arguments
-p = inputParser;
+%% Check input and default arguments
 
-defaultAction = 'replace';
+validChannelNames = {'Salinity', 'Temperature', 'Conductivity', 'Chlorophyll', 'Dissolved O', 'CDOM', 'Turbidity'};
+checkChannelName = @(x) any(validatestring(x,validChannelNames));
+
+validSeries = {'profile', 'data'};
+checkSeriesName = @(x) any(validatestring(x,validSeries));
+
+validDirections = {'up', 'down'};
+checkDirection = @(x) any(validatestring(x,validDirections));
+
 validActions = {'replace','NaN'};
 checkAction = @(x) any(validatestring(x,validActions));
 
-addRequired(p,'x', @isnumeric);
+%% Parse Inputs
+
+p = inputParser;
+addParameter(p,'channel', 'temperature', checkChannelName);
+addParameter(p,'series', 'data', checkSeriesName);
+addParameter(p,'profileNum', [], @isnumeric)
 addParameter(p,'n', 4, @isnumeric);
 addParameter(p,'k', 7, @isnumeric);
-addParameter(p,'action', defaultAction', checkAction);
-
-parse(p,x,varargin{:})
+addParameter(p,'action', 'replace', checkAction);
+addParameter(p,'direction', 'down', checkDirection);% Only needed if series is 'profile'
+parse(p, varargin{:})
 
 %Assign each argument
+channel = p.Results.channel;
+series = p.Results.series;
+direction = p.Results.direction;
+profileNum = p.Results.profileNum;
 n = p.Results.n;
 k = p.Results.k;
 action = p.Results.action;
 
+%% Select Time Series
+if strcmp(series,'profile') & strcmp(p.UsingDefaults,'profileNum')
+    switch direction
+      case 'down'
+        profileNum = 1:length(RSK.profiles.downcast.data);
+      case 'up'
+        profileNum = 1:length(RSK.profiles.upcast.data);
+    end
+end
+
+channelCol = find(strncmpi(channel, {RSK.channels.longName}, 4));
+channelCol = channelCol(1); % If there are two pick the first. Usually temp or pressure.
+
+%% Despike
+switch series
+    case 'profile'
+        switch direction
+            case 'up'
+                for i=profileNum
+                    x = RSK.profiles.upcast.data(i).values(:,channelCol);
+                    RSK.profiles.upcast.data(i).values(:, channelCol) = despike(x, n, k, action); 
+                end
+            case 'down'
+                for i=profileNum
+                    x = RSK.profiles.downcast.data(i).values(:,channelCol);
+                    RSK.profiles.downcast.data(i).values(:, channelCol) = despike(x, n, k, action); 
+                end
+        end
+        
+    case 'data'
+        x = RSK.data.values(:,channelCol);
+        RSK.data.values(:, channelCol) = despike(x, n, k, action); 
+end
+end
+
+
+%% Nested Functions
+function [y] = despike(x, n, k, action)
 y = x;
 ref = runmed(x, k);
 dx = x - ref;
@@ -68,7 +137,6 @@ switch action
   case 'NaN'
     y(I) = NaN;
 end
-
 end
 
 function out = runmed(in, k)
