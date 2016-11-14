a698fcda9880a47c41eb7d99052cdea08d34380f
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-11-14 16:52:05 -0500

    Allow for different methods of chosing bin size. Argo regimes bin size is an option
    RSKTOOLS-41

--- RSKbinAveraging.m
+++ RSKbinAveraging.m
@@ -11,11 +11,24 @@ function [Y, binCenter] = RSKbinAveraging(X, Pressure, varargin)
 %   [Required] - X - the profile of the channel to be binned.
 %
 %                P - Pressure array corresponding to X.
+%   
+%   [Optional] - Method - The way to find bin values. 'Width' set the bin array based on a
+%                   binWidth parameter, it is the default. 'Array' sets the
+%                   bin array to be the array entered in the 'binArray'
+%                   parameter. 'ArgoRegimes' uses the binsize regimes
+%                   that the Argo floats use, more information: 
+%                   http://docs.rbr-global.com/L2commandreference/quick-start/integrating-with-a-profiling-float
+% 
+%                binBy - The array it will be bin wrt... Depth or Pressure.
+%                   Defaults to 'pressure'. Not applicable when 'Method' is
+%                   'ArgoRegimes'.
 %
-%   [Optional] - binBy - The array it will be bin wrt... Depth or Pressure.
-%                Defaults to 'pressure'.
+%                binArray - An array that determines the bin limits.
+%                   Default is [0.5:1:100]. Only used when 'Method' is
+%                   'Array'.
 %
-%                binWidth - the width of the bin. Default is 1.
+%                binWidth - the width of the bin. Default is 1. Only used
+%                   when 'Method' is 'Width'.
 %
 % Outputs:
 %    Y - Binned array
@@ -27,51 +40,90 @@ function [Y, binCenter] = RSKbinAveraging(X, Pressure, varargin)
 % Last revision: 2016-11-10
 
 %% Input Parse
-
 validBinBy = {'pressure', 'depth'};
 checkBinBy = @(x) any(validatestring(x,validBinBy));
+
+validMethods = {'Width', 'Array', 'ArgoRegimes'};
+checkMethod = @(x) any(validatestring(x,validMethods));
 
 %% Parse Inputs
 
 p = inputParser;
 addRequired(p, 'X', @isnumeric);
 addRequired(p, 'Pressure', @isnumeric);
+addParameter(p, 'Method', 'Width', checkMethod)
 addParameter(p, 'binBy', 'pressure', checkBinBy);
+addParameter(p, 'binArray', (0.5:1:100), @isnumeric);
 addParameter(p, 'binWidth', 1, @isnumeric);
 parse(p, X, Pressure, varargin{:})
 
 % Assign each argument
 X = p.Results.X;
 Pressure = p.Results.Pressure;
+Method = p.Results.Method;
 binBy = p.Results.binBy;
+binArray = p.Results.binArray;
 binWidth = p.Results.binWidth;
 
 %% Binning 
+Depth = -gsw_z_from_p(Pressure,52);
 
-switch binBy
-  case 'pressure'
-    binCenter = [binWidth:binWidth:ceil(max(Pressure))]';
-  case 'depth'
-    Depth = -gsw_z_from_p(Pressure,52);
-    binCenter = [binWidth:binWidth:ceil(max(Depth))]';
+switch Method
+    case 'Width' %Should output bins for this case
+        switch binBy
+          case 'pressure'
+            binCenter = (binWidth:binWidth:ceil(max(Pressure)))';
+          case 'depth'
+            binCenter = (binWidth:binWidth:ceil(max(Depth)))';
+        end
+
+        %  initialize the binned output field  
+        Y = NaN(length(binCenter),1);
+
+        for k=1:length(binCenter)
+
+          switch binBy
+            case 'pressure'
+              kk = (Pressure >= binCenter(k)-binWidth/2) & (Pressure < binCenter(k)+binWidth/2);
+            case 'depth'
+              kk = (Depth >= binCenter(k)-binWidth/2) & (Depth < binCenter(k)+binWidth/2);
+          end
+          Y(k) = nanmean(X(kk));
+        end
+        
+    case 'Array'
+        bins = binArray;
+        binCenter = tsmovavg(bins, 's', 2);
+        binCenter = binCenter(2:end); %Starts with NaN.
+        %  initialize the binned output field         
+        Y = NaN(length(bins)-1,1);
+        for k=1:length(bins)-1
+          switch binBy
+            case 'pressure'            
+                kk = Pressure >= bins(k) & Pressure < bins(k+1);
+            case 'depth'
+                kk = Depth >= bins(k) & Depth < bins(k+1);
+          end
+            Y(k) = nanmean(X(kk));
+        end
+        
+    case 'ArgoRegimes'
+        if strcmp(binBy, 'depth'), warning('Must be binned by Pressure...will bin by pressure'); end
+        bins = [10, 50, 60:20:200, 250:50:500];
+        binCenter = tsmovavg(bins, 's', 2);
+        binCenter = binCenter(2:end); %Starts with NaN.
+        
+        %  initialize the binned output field         
+        Y = NaN(length(bins)-1,1);
+        for k=1:length(bins)-1
+            kk = Pressure >= bins(k) & Pressure < bins(k+1);
+            Y(k) = nanmean(X(kk));
+        end
+        
 end
-
-%  initialize the binned output field  
-Y = NaN(length(binCenter),1);
-
-for k=1:length(binCenter)
-    
-  switch binBy
-    case 'pressure'
-      kk = (Pressure >= binCenter(k)-binWidth/2) & (Pressure < binCenter(k)+binWidth/2);
-    case 'depth'
-      kk = (Depth >= binCenter(k)-binWidth/2) & (Depth < binCenter(k)+binWidth/2);
-  end
   
-  Y(k) = nanmean(X(kk));
-  
 end
-end
+
 
 
 
