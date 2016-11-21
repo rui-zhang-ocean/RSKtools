63c1d3db477c9639e2b50781dca2ac4323ffe292
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-11-21 14:38:02 -0500

    Fix coefficients field
    RSKTOOLS-46 #comment I was able to get the calibrations table to work but the events are inconsistent through versions and the customers have documentation to use them. Unless they are using them I would rather leave them out of the struct created by RSK2MAT.m

--- RSK2MAT.m
+++ RSK2MAT.m
@@ -23,7 +23,7 @@ function [RBR] = RSK2MAT(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-11-08
+% Last revision: 2016-11-21
 
 try 
     firmwareV  = RSK.deployments.firmwareVersion;
@@ -40,12 +40,21 @@ RBR.channelranging = {RSK.ranging.mode}';
 RBR.starttime = datestr(RSK.epochs.startTime, 'dd/mm/yyyy HH:MM:SS PM');
 RBR.endtime = datestr(RSK.epochs.endTime, 'dd/mm/yyyy HH:MM:SS PM');
 
-%If many channels have the same ID the calibration tables isn't consistent.
-%RBR.coefficients = [{RSK.calibrations(1:8).c0};{RSK.calibrations(1:8).c1};{RSK.calibrations(1:8).c2};{RSK.calibrations(1:8).c3}];
+% Set up coefficients table
+nchannels = length(RBR.channelnames);
+RBR.coefficients = zeros(4, nchannels);
+for i=1:nchannels
+    channelindex = find([RSK.calibrations.channelOrder] == i);
+    coefcell = [{RSK.calibrations(channelindex(end)).c0}; {RSK.calibrations(channelindex(end)).c1}; {RSK.calibrations(channelindex(end)).c2; RSK.calibrations(channelindex(end)).c3}];
+    nocoef = cellfun('isempty', coefcell);
+    coefcell(nocoef) = {NaN};
+    RBR.coefficients(:,i) = cell2mat(coefcell);
+end
+
 RBR.sampletimes = cellstr(datestr(RSK.data.tstamp, 'yyyy-mm-dd HH:MM:ss.FFF'));
 RBR.data = RSK.data.values;
 
-% Events...Not consistent
+% %Events...Not consistent
 % if isfield(RSK, 'events')
 %     k = find(RSK.events.values(:,4) == 0);
 %     x1 = cellstr(strcat('DIAG-',num2str(RSK.events.values(k,2))));
@@ -53,6 +62,6 @@ RBR.data = RSK.data.values;
 %     x3 = cellstr(num2str(RSK.events.values(k,3)));
 %     RBR.events = [x1, x2, x3];
 % end
-    
+%     
 
 end
