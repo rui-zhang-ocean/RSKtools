344919533330db28906e13322d60bc9fec7939b8
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-11-28 15:36:45 -0500

    Set cast direction variable

--- RSKloopedit.m
+++ RSKloopedit.m
@@ -1,17 +1,21 @@
 function [RSK] = RSKloopedit(RSK, varargin)
 
-% RSKloopedit - De-spike a time series using a running median filter.
+% RSKloopedit - Flags values that have pressure reversal or slowdows during
+%               the profile.
 %
 % Syntax:  [RSK] = RSKloopedit(RSK, [OPTIONS])
 % 
-% RSKprofileedit - This function flags scans that have a low profiling
-% velocity or decelerating  and replaces them with a interpolated value or
-% NaN. It operates on two scans to determine the velocity and two
-% velocities for the acceleration, it is best to perform RSKfilter.m on the
-% pressure channel before because the noise will significantly disrupt the velocity.
+% RSKloopedit - This function flags scans that have a low profiling
+% velocity or decelerating below a specified threshold and replaces them
+% with a NaN or an interpolated value. It operates on two scans to
+% determine the velocity and two velocities for the acceleration.
+%
+% Note: It is VERY important to run RSKfilter.m on the
+% pressure channel to filter out the noise before running RSKloopedit
+% because the noise will be amplified by this function. 
 %
 % Inputs:
-%    
+%
 %   [Required] - RSK - the input RSK structure, with profiles as read using
 %                    RSKreadprofiles
 %
@@ -28,11 +32,15 @@ function [RSK] = RSKloopedit(RSK, varargin)
 %                   be taken. Default is 0.25 m/s
 %
 %                minDecel - the minimum deceleration at which the profile must
-%                   be taken.Default 'None'.
+%                   be taken. Default 'None'.
 %
 %                action - the 'action' to perform on a flagged value. The
-%                   default, is to 'interp', to interpolate based on 'good'
-%                   values. 'NaN' to leave the spike as a missing value.
+%                   default, 'NaN', is to leave the spike as a missing
+%                   value. Another option 'interp' is to interpolate based
+%                   on 'good' values. 
+%
+%                latitude - Latitude at which the profile was taken.
+%                   Default is 52.
 %
 % Outputs:
 %    RSK - the structure without pressure reversal or slowdowns
@@ -42,13 +50,12 @@ function [RSK] = RSKloopedit(RSK, varargin)
 %    RSK = RSKreadprofiles(RSK)
 %    RSK = RSKfilter(RSK, 'pressure')
 %    RSK = RSKalignchannel(RSK, 'salinity')
-%    RSK = RSKprofileedit(RSK)
-%    temperatureDS = RSKdespike(RSK, 'threshold',2, 'windowLength',10, 'action','NaN');
+%    RSK = RSKloopedit(RSK)
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-11-24
+% Last revision: 2016-11-28
 
 %% Check input and default arguments
 
@@ -58,6 +65,9 @@ checkDirection = @(x) any(validatestring(x,validDirections));
 validActions = {'NaN', 'interp'};
 checkAction = @(x) any(validatestring(x,validActions));
 
+classes = {'double'};
+attributes = {'>=',-90,'<=',90};
+checkLatitude = @(x) any(validateattributes(x, classes, attributes));
 
 
 %% Parse Inputs
@@ -69,7 +79,7 @@ addParameter(p, 'profileNum', [], @isnumeric);
 addParameter(p, 'minVelocity', 0.25, @isnumeric);
 addParameter(p, 'minDecel', 'None', @isnumeric);
 addParameter(p, 'action', 'NaN', checkAction);
-addParameter(p, 'latitude', 52, @isnumeric); %Enforce lat to be within lat values....
+addParameter(p, 'latitude', 52, checkLatitude); 
 parse(p, RSK, varargin{:})
 
 % Assign each argument
@@ -84,7 +94,7 @@ latitude = p.Results.latitude;
 
 
 %% Determine if the structure has downcasts and upcasts & set profileNum accordingly
-
+castdir = [direction 'cast'];
 isDown = isfield(RSK.profiles.downcast, 'data');
 isUp   = isfield(RSK.profiles.upcast, 'data');
 switch direction
@@ -109,15 +119,8 @@ end
 pressureCol = find(strcmpi('pressure', {RSK.channels.longName}));
 secondsperday = 86400;
 for i = profileNum
-    switch direction
-        case 'up' 
-            depth = -gsw_z_from_p(RSK.profiles.upcast.data(i).values(:,pressureCol), latitude);
-            time = RSK.profiles.upcast.data(i).tstamp;
-            
-        case 'down'    
-            depth = -gsw_z_from_p(RSK.profiles.downcast.data(i).values(:,pressureCol), latitude);
-            time = RSK.profiles.downcast.data(i).tstamp;
-    end
+    depth = -gsw_z_from_p(RSK.profiles.(castdir).data(i).values(:,pressureCol), latitude);
+    time = RSK.profiles.(castdir).data(i).tstamp;
     
     %% Caculate Velocity.
     deltaD = diff(depth);
@@ -143,28 +146,14 @@ for i = profileNum
    
     %% Perform the action on flagged scans.
     flagChannels = ~strcmpi('pressure', {RSK.channels.longName});    
-    switch direction
-        case 'up'
-            switch action
-                case 'NaN' 
-                    RSK.profiles.upcast.data(i).values(flag,flagChannels) = NaN;
-                case 'interp'
-                    for k = find(flagChannels)
-                        RSK.profiles.upcast.data(i).values(:,k) = interp1(time(~flag), RSK.profiles.upcast.data(i).values(~flag,k), time, 'linear', 'extrap');
-                    end
+    switch action
+        case 'NaN' 
+            RSK.profiles.(castdir).data(i).values(flag,flagChannels) = NaN;
+        case 'interp'
+            for k = find(flagChannels)
+                RSK.profiles.(castdir).data(i).values(:,k) = interp1(time(~flag), RSK.profiles.(castdir).data(i).values(~flag,k), time, 'linear', 'extrap');
             end
-            
-        case 'down'
-            switch action
-                case 'NaN' 
-                    RSK.profiles.downcast.data(i).values(flag,flagChannels) = NaN;
-                case 'interp'
-                    for k = find(flagChannels)
-                        RSK.profiles.downcast.data(i).values(:,k) = interp1(time(~flag), RSK.profiles.downcast.data(i).values(~flag,k), time, 'linear', 'extrap');
-                    end
-            end
-    end
-                        
+    end                 
 
 end
 end
