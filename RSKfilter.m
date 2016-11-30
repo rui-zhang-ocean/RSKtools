2bd248c0389c63e023885319ac4e204a4ddf6ec9
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-11-30 11:30:44 -0500

    Add option to use filtfilt
    RSKTOOLS-44

--- RSKfilter.m
+++ RSKfilter.m
@@ -1,4 +1,4 @@
-function RSK = RSKfilter(RSK, channel, filter, varargin)
+function RSK = RSKfilter(RSK, channel, varargin)
 
 % RSKfilter - Low pass filter on specified channels
 %
@@ -16,10 +16,11 @@ function RSK = RSKfilter(RSK, channel, filter, varargin)
 %                 channel - Longname of channel to filter, Can be cell
 %                    array of many channels
 %               
-%                 filter - Types... *Suggestions of times/windowsize based
-%                    on purpose and snesor?
-%git statu
-%    [Optional] - profileNum - the profiles to which to apply the correction. If
+%    [Optional] - filter - Default is boxcar, a simple moving average.
+%                   Another option is filtfilt, requiers numerator and
+%                   denomenator coefficients
+%               
+%                 profileNum - the profiles to which to apply the correction. If
 %                    left as an empty vector, will do all profiles.
 %            
 %                 direction - the profile direction to consider. Must be either
@@ -29,20 +30,23 @@ function RSK = RSKfilter(RSK, channel, filter, varargin)
 %                    be applied span-1/2 scans to the left and right of the
 %                    center value....
 %
+%                 coefficients - a vector with the [numerator, denominator]
+%                    as the filtfilt coefficients.
 %
 % Outputs:
-%    RSK - the RSK structure with aligned channel values
+%    RSK - the RSK structure with filtered channel values.
 %
 % Example: 
 %   
 %    rsk = RSKopen('file.rsk');
 %    rsk = RSKreadprofiles(rsk, 1:10); % read first 10 downcasts
+%    rsk = RSKfilter(rsk, {'Temperature', 'Salinity'}, 'span', 10);
 %
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-11-15
+% Last revision: 2016-11-30
 
 %% Check input and default arguments
 
@@ -52,7 +56,7 @@ checkChannelName = @(x) any(validatestring(x,validChannelNames));
 validDirections = {'down', 'up'};
 checkDirection = @(x) any(validatestring(x,validDirections));
 
-validFilterNames = {'boxcar'};
+validFilterNames = {'boxcar', 'filtfilt'};
 checkFilterName = @(x) any(validatestring(x,validFilterNames));
 
 
@@ -61,12 +65,13 @@ checkFilterName = @(x) any(validatestring(x,validFilterNames));
 p = inputParser;
 addRequired(p, 'RSK', @isstruct);
 addRequired(p, 'channel', checkChannelName);
-addRequired(p, 'filter', checkFilterName);
+addParameter(p, 'filter', 'boxcar', checkFilterName);
 addParameter(p, 'profileNum', [], @isnumeric);
 addParameter(p, 'direction', 'down', checkDirection);
 addParameter(p, 'span', 3, @isnumeric)
+addParameter(p, 'coefficients', [1 1])
 
-parse(p, RSK, channel, filter, varargin{:})
+parse(p, RSK, channel, varargin{:})
 
 % Assign each input argument
 RSK = p.Results.RSK;
@@ -75,6 +80,7 @@ filter = p.Results.filter;
 profileNum = p.Results.profileNum;
 direction = p.Results.direction;
 span = p.Results.span;
+coefficients = p.Results.coefficients;
 
 
 
@@ -107,17 +113,12 @@ for chanName = channel
             for i = profileNum
                 RSK.profiles.(castdir).data(i).values(:,channelCol) = smooth(RSK.profiles.(castdir).data(i).values(:,channelCol), span);
             end
-
-        case 'sampling rate'
-            %Pressure has been filtered propotional to the sampling period. 
-            %designfilter('timeconstant', rsk.schedule.samplingperiode/1000);
-            %filtfilt('lowpassfir');
-        case 'time constant'
-            % filter based on sensor time constant? Would that determine the
-            % frequency cutoff? 
-
-       %By filter type
-
+        case 'filtfilt'
+            for i = profileNum
+                b = coefficients(1);
+                a = coefficients(2);
+                RSK.profiles.(castdir).data(i).values(:,channelCol) = filtfilt(b, a, RSK.profiles.(castdir).data(i).values(:,channelCol));
+            end
     end
         
 end
