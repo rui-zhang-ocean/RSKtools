2f80d8b9dea13560f2213569352b7a0307852b84
Muriel Dunn <muriel.dunn@rbr-global.com>
2016-12-20 17:05:13 -0500

    Remove geodata table if it is empty

--- readheaderEP.m
+++ readheaderEP.m
@@ -5,15 +5,15 @@ function RSK = readheaderEP(RSK)
 % Syntax:  [RSK] = readheaderEP(RSK)
 %
 % readheaderEP is a RSKtools helper function that opens the populated
-% tables of 'EasyParse' files. Only to be used by RSKopen.m.
-% These tables are channels, epochs, schedules and deployments. If available
-% there will also be geodata.
+% tables of 'EasyParse' files.
+% These tables are channels, epochs, schedules and deployments. If data is
+% available it will also open geodata.
 %
 % Inputs:
 %    RSK - 'EasyParse' file opened using RSKopen.m
 %
 % Outputs:
-%    RSK - Structure containing the logger metadata and thumbnails
+%    RSK - Structure containing the logger metadata
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
@@ -31,13 +31,17 @@ RSK.schedules = mksqlite('select * from schedules');
 
 RSK.deployments = mksqlite('select * from deployments');
 
-%% Tables that may or may not be in 'EasyParse'
+%% Tables that could be populated in 'EasyParse'
 try
     UTCdelta = mksqlite('select UTCdelta/1.0 as UTCdelta from epochs');
     RSK.epochs.UTCdelta = UTCdelta.UTCdelta;
     RSK.geodata = mksqlite('select tstamp/1.0 as tstamp, latitude, longitude, accuracy, accuracyType from geodata');
-    for ndx = 1:length(RSK.geodata)
-        RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + RSK.epochs.UTCdelta);
+    if isempty(RSK.geodata)
+        RSK = rmfield(RSK, 'geodata');
+    else
+        for ndx = 1:length(RSK.geodata)
+            RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + RSK.epochs.UTCdelta);
+        end
     end
 catch 
 end
