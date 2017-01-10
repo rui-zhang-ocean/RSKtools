83837be39ebb427d52989b1c3faf83cd20e6dfc1
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-01-10 10:13:50 -0500

    separate profile event reading into a different function

--- RSKopen.m
+++ RSKopen.m
@@ -37,7 +37,7 @@ function [RSK, dbid] = RSKopen(fname)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-12-19
+% Last revision: 2017-01-10
 
 RSKconstants
 
@@ -83,40 +83,9 @@ switch RSK.dbInfo(end).type
 end
 
 
-%% Want to read in events so that we can get the profile event metadata
-% 
-% FIXME: what happens when there are no profile events? Should just skip this
+%% Read in events so that we can get the profile event metadata
 
-try 
-    tmp = RSKreadevents(RSK);
-    events = tmp.events;
-catch
-end
+RSK = RSKgetprofiles(RSK);
 
-if exist('events', 'var')
-    nup = length(find(events.values(:,2) == eventBeginUpcast));
-    ndown = length(find(events.values(:,2) == eventBeginDowncast));
-    
-    if ~(nup == 0 && ndown == 0)
-        
-        iup = find(events.values(:,2) == eventBeginUpcast);
-        idown = find(events.values(:,2) == eventBeginDowncast);
-        iend = find(events.values(:,2) == eventEndcast);
-        
-        % which is first?
-        if (idown(1) < iup(1)) 
-            idownend = iend(1:2:end);
-            iupend = iend(2:2:end);
-        else
-            idownend = iend(2:2:end);
-            iupend = iend(1:2:end);
-        end
-        
-        RSK.profiles.downcast.tstart = events.tstamp(idown);
-        RSK.profiles.downcast.tend = events.tstamp(idownend);
-        RSK.profiles.upcast.tstart = events.tstamp(iup);
-        RSK.profiles.upcast.tend = events.tstamp(iupend);
-        
-    end
 
 end
