385e47d5d822aeb4bcb71030438aac56a7587f34
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-01-11 14:44:43 -0500

    Add option to enter UTCdelta, if one is entered it will be used.
    RSKTOOLS-65 #resolve Added option to add UTCdelta.

--- RSKreadgeodata.m
+++ RSKreadgeodata.m
@@ -1,4 +1,4 @@
-function RSK = RSKreadgeodata(RSK)
+function RSK = RSKreadgeodata(RSK, varargin)
 
 % RSKreadgeodata - Reads the geodata of a rsk file
 %
@@ -9,6 +9,9 @@ function RSK = RSKreadgeodata(RSK)
 % Inputs:
 %    RSK - Structure containing the logger metadata and thumbnails
 %          returned by RSKopen.
+%    UTCdelta - The offset of the timestamp. If a value is entered it will
+%          be used. Otherwise it will use the one given in the epochs
+%          table, if there is none it will use 0.
 %
 % Output:
 %    RSK - Structure containing previously present logger metadata as well
@@ -17,22 +20,39 @@ function RSK = RSKreadgeodata(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2016-12-22
+% Last revision: 2017-01-11
 
+%% Parse Inputs
 
+p = inputParser;
+addRequired(p, 'RSK', @isstruct);
+addParameter(p, 'UTCdelta', 0);
+parse(p, RSK, varargin{:})
+
+% Assign each input argument
+RSK = p.Results.RSK;
+UTCdelta = p.Results.UTCdelta;
+
+%% Read geodata
 RSK.geodata = mksqlite('select tstamp/1.0 as tstamp, latitude, longitude, accuracy, accuracyType from geodata');
 if isempty(RSK.geodata)
     RSK = rmfield(RSK, 'geodata');
-else
+elseif strcmpi(p.UsingDefaults, 'UTCdelta')
     try
         UTCdelta = mksqlite('select UTCdelta/1.0 as UTCdelta from epochs');
         RSK.epochs.UTCdelta = UTCdelta.UTCdelta;
+        for ndx = 1:length(RSK.geodata)
+            RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + RSK.epochs.UTCdelta);
+        end    
     catch
         warning('No UTCdelta value, the timestamps in geodata cannot be adjust to the logger time, will use 0');
-        UTCdelta.UTCdelta = 0;
+        for ndx = 1:length(RSK.geodata)
+            RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp);
+        end
     end
+else
     for ndx = 1:length(RSK.geodata)
-        RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + UTCdelta.UTCdelta);
+        RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + UTCdelta);
     end
 end
 end
