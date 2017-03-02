2c71d9cb2715420d8c0680c53976711b4a856830
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-03-02 09:46:30 -0500

    EPdesktop always has instrumentchannels

--- readheaderEPdesktop.m
+++ readheaderEPdesktop.m
@@ -38,6 +38,7 @@ RSK.schedules = mksqlite('select * from schedules');
 RSK.deployments = mksqlite('select * from deployments');
 
 RSK.instruments = mksqlite('select * from instruments');
+RSK.instrumentChannels = mksqlite('select * from instrumentChannels');
 
 RSK.thumbnailData = RSKreadthumbnail;
 
