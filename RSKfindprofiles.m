d5a1f7b69402d120598d7128b5e0fc3a72e99724
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-03-08 14:20:55 -0500

    Establish start and end times of profiles based on events detected by detectprofiles
    RSKTOOLS-67

--- RSKfindprofiles.m
+++ RSKfindprofiles.m
@@ -12,6 +12,10 @@ function RSK = RSKfindprofiles(RSK, varargin)
 %   RSK - the input RSK structure, with profiles but no
 %                   profile events
 %
+% Output: 
+%   RSK - Structure containing profiles field with the profile metadata.
+%         Use RSKreadprofiles to populate the profiles field with data.
+%
 % Ex:
 %    RSK = RSKopen(fname);
 %    RSK = RSKreaddata(RSK);
@@ -20,11 +24,10 @@ function RSK = RSKfindprofiles(RSK, varargin)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-01-16
+% Last revision: 2017-03-08
 
 
 %% Parse Inputs
-
 p = inputParser;
 addRequired(p, 'RSK', @isstruct);
 addParameter(p, 'profileThreshold', [], @ isnumeric);
@@ -33,6 +36,7 @@ parse(p, RSK, varargin{:})
 % Assign each argument
 RSK = p.Results.RSK;
 profileThreshold = p.Results.profileThreshold;
+
 
 %% Check if upcasts is already populated
 if isfield(RSK, 'profiles')
@@ -44,36 +48,71 @@ end
 pressureCol = find(strcmpi('pressure', {RSK.channels.longName}));
 condCol = find(strcmpi('conductivity', {RSK.channels.longName}));
 pressure = RSK.data.values(:, pressureCol(1));
+timestamp = RSK.data.tstamp;
+
+% If conductivity is present it will be used to detect when the logger is
+% out of the water.
 if isempty(condCol)
     conductivity = [];
 else 
     conductivity = RSK.data.values(:, condCol(1));
 end
-timestamp = RSK.data.tstamp;
+
+% Profile threshold is a quarter of the max range of pressure if none is input.
 if isempty(profileThreshold)
     profileThreshold = (max(pressure)-min(pressure))/4;
 end
 
 
-
 %% Run profile detection
-[upcaststart, downcaststart] = detectprofiles(pressure, timestamp, conductivity, profileThreshold);
-if ~any(size(upcaststart)>1 & size(downcaststart)>1)
-    return;
-elseif upcaststart(1) < downcaststart(1)
-    RSK.profiles.upcast.tstart = upcaststart;
-    RSK.profiles.upcast.tend = downcaststart;
-    RSK.profiles.downcast.tstart = downcaststart;
-    RSK.profiles.downcast.tend = upcaststart(2:end);
-    RSK.profiles.downcast.tend(end+1) = RSK.data.tstamp(end);
-else
-    RSK.profiles.upcast.tstart = upcaststart;
-    RSK.profiles.upcast.tend = downcaststart(2:end);
-    RSK.profiles.upcast.tend(end+1) = RSK.data.tstamp(end);
-    RSK.profiles.downcast.tstart = downcaststart;
-    RSK.profiles.downcast.tend = upcaststart;
+[wwevt] = detectprofiles(pressure, timestamp, conductivity, profileThreshold);
+
+
+%% Use the events to establish profile start and end times.
+% Event 1 is a downcast start
+downstart = wwevt(wwevt(:,2) == 1,1);
+% Event 2 is a upcast start
+upstart = wwevt(wwevt(:,2) == 2,1);
+% Event 3 is out of water
+
+u=1;% up index
+d=1;% down index
+for ndx = 2:length(wwevt)
+    t = find(timestamp == wwevt(ndx,1));
+    if wwevt(ndx-1,2) ~= 3
+        if wwevt(ndx,2) == 1
+            % Upcast end is the sample before a downcast start
+            upend(u) = timestamp(t-1);
+            u = u+1;
+
+        elseif wwevt(ndx,2) == 2
+            % Downcast end is the sample before a upcast start
+            downend(d) = timestamp(t-1);
+            d = d+1;  
+        end
+
+    end
+    if wwevt(ndx,2) == 3
+        if wwevt(ndx-1,2) == 1
+            % Event 3 ends a downcast if that was the last event
+            downend(d) = timestamp(t-1);
+            d = d+1;
+            
+         elseif wwevt(ndx-1,2) == 2
+             % Event 3 ends a upcast if that was the last event
+            upend(u) = timestamp(t-1);
+            u = u+1;
+        end
+    end
 end
-    
+         
+
+%% Put profiling events into RSK profiles field.
+RSK.profiles.upcast.tstart = upstart;
+RSK.profiles.upcast.tend = upend';
+RSK.profiles.downcast.tstart = downstart;
+RSK.profiles.downcast.tend = downend';
+ 
 
 end
             
