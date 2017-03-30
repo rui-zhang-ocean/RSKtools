f3372ecc51955258c38ae5529d6dc9dd89e85dc2
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-03-30 10:36:00 -0400

    Improve CTP channel check
    RSKTOOLS-27

--- RSKderivesalinity.m
+++ RSKderivesalinity.m
@@ -27,11 +27,11 @@ data = RSK.data;
 %% Check TEOS-10 and CTP data are available.
  
 if isempty(which('gsw_SP_from_C'))
-    error('RSKtools required TEOS-10 toolbox to derive salinity. Download it here: http://www.teos-10.org/software.htm')
+    error('RSKtools requires TEOS-10 toolbox to derive salinity. Download it here: http://www.teos-10.org/software.htm');
 end
     
-if ~strcmpi(RSK.channels(1).longName, 'Conductivity') && ~strcmpi(RSK.channels(2).longName, 'Temperature') && ~strcmpi(RSK.channels(3).longName, 'Pressure');
-    error('Conductivity, Temperature and Pressure are required to calculate Salinity')
+if length(RSK.channels) < 3 || (~any(strcmpi({RSK.channels.longName}, 'Conductivity')) && ~any(strcmpi({RSK.channels.longName}, 'Temperature')) && ~any(strcmpi({RSK.channels.longName}, 'Pressure')))
+    error('Conductivity, Temperature and Pressure are required to calculate Salinity');
 end
 
 
@@ -53,14 +53,12 @@ if ~hasS
         RSK.instrumentChannels(nchannels+1).channelID = RSK.instrumentChannels(nchannels).channelID+1;
         RSK.instrumentChannels(nchannels+1).channelOrder = RSK.instrumentChannels(nchannels).channelOrder+1;
     end
-    salinity = gsw_SP_from_C(data.values(:, 1), data.values(:, 2), data.values(:, 3)- 10.1325);
-    RSK.data.values = [data.values salinity];
-    
-elseif hasS
-    Scol = strcmp({RSK.channels.longName}, 'Salinity');
-    salinity = gsw_SP_from_C(data.values(:, 1), data.values(:, 2), data.values(:, 3)- 10.1325);
-    RSK.data.values(:,Scol) = salinity;
 end
+
+Scol = strcmp({RSK.channels.longName}, 'Salinity');
+salinity = gsw_SP_from_C(data.values(:, 1), data.values(:, 2), data.values(:, 3)- 10.1325);
+RSK.data.values(:,Scol) = salinity;
+
 
 
 end
