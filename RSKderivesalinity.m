8650ab3c7e88929320cb9a6c4beba2d510d36fc8
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-03-30 15:02:07 -0400

    Add ability to derive salinity of profiles
    RSKTOOLS-27

--- RSKderivesalinity.m
+++ RSKderivesalinity.m
@@ -1,18 +1,26 @@
-function [RSK, salinity] = RSKderivesalinity(RSK)
+function [RSK, salinity] = RSKderivesalinity(RSK, varargin)
 
 % RSKderivesalinity - Calculate salinity and add it or replace it in the data table
 %
 % Syntax:  [RSK] = RSKderivesalinty(RSK, [OPTIONS])
 % 
 % This function derives salinity using the TEOS-10 toolbox and fills the
-% appropriate fields in channels field and data field. If salinity is
+% appropriate fields in channels field and data or profile field. If salinity is
 % already calculated, it will recalculate it and overwrite that data
 % column. 
 % This function requires TEOS-10 to be downloaded and in the path
 % (http://www.teos-10.org/software.htm)
 %
-% Inputs:
-%    RSK - the input RSK structure
+%
+% Inputs: 
+%    [Required] - RSK - Structure containing the logger metadata and data
+%
+%               
+%    [Optional] - series - Specifies the series to be filtered. Either 'data'
+%                     or 'profile'. Default is 'data'.
+%            
+%                 direction - 'up' for upcast, 'down' for downcast, or 'both' for
+%                     all. Default is 'down'.
 %
 % Outputs:
 %    RSK - RSK structure containing the salinity data
@@ -20,9 +28,41 @@ function [RSK, salinity] = RSKderivesalinity(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-03-27
+% Last revision: 2017-03-30
 
-data = RSK.data;
+
+
+%% Check input and default arguments
+
+validSeries = {'profile', 'data'};
+checkSeriesName = @(x) any(validatestring(x,validSeries));
+
+validDirections = {'down', 'up'};
+checkDirection = @(x) any(validatestring(x,validDirections));
+
+
+%% Parse Inputs
+
+p = inputParser;
+addRequired(p, 'RSK', @isstruct);
+addParameter(p, 'series', 'data', checkSeriesName)
+addParameter(p, 'direction', 'down', checkDirection);
+parse(p, RSK, varargin{:})
+
+% Assign each input argument
+RSK = p.Results.RSK;
+series = p.Results.series;
+direction = p.Results.direction;
+
+
+%% Determine if the structure has downcasts and upcasts
+
+if strcmpi(series, 'profile')
+    profileNum = [];
+    profileIdx = checkprofiles(RSK, profileNum, direction);
+    castdir = [direction 'cast'];
+end
+
 
 %% Check TEOS-10 and CTP data are available.
  
@@ -55,11 +95,25 @@ if ~hasS
     end
 end
 
-Scol = strcmp({RSK.channels.longName}, 'Salinity');
-salinity = gsw_SP_from_C(data.values(:, 1), data.values(:, 2), data.values(:, 3)- 10.1325);
-RSK.data.values(:,Scol) = salinity;
 
 
+Scol = strcmpi({RSK.channels.longName}, 'Salinity');
+Ccol = strcmpi({RSK.channels.longName}, 'Conductivity');
+Tcol = strcmpi({RSK.channels.longName}, 'Temperature');
+Pcol = strcmpi({RSK.channels.longName}, 'Pressure');
+
+switch series
+    case 'data'
+        data = RSK.data;
+        salinity = gsw_SP_from_C(data.values(:, Ccol), data.values(:, Tcol), data.values(:, Pcol)- 10.1325);
+        RSK.data.values(:,Scol) = salinity;
+    case 'profile'
+        for ndx = profileIdx
+            data = RSK.profiles.(castdir).data(ndx);
+            salinity = gsw_SP_from_C(data.values(:, Ccol), data.values(:, Tcol), data.values(:, Pcol)- 10.1325);
+            RSK.profiles.(castdir).data(ndx).values(:,Scol) = salinity;
+        end
+end
 
 end
 
