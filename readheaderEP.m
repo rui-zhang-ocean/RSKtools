277aed146b1016ab263308341dde5da2b2dcf2b8
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-03-31 12:01:36 -0400

    reverse changes

--- readheaderEP.m
+++ readheaderEP.m
@@ -18,7 +18,20 @@ function RSK = readheaderEP(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-03-31
+% Last revision: 2017-03-29
+
+%% Tables that are definitely in 'EasyParse'
+RSK.channels = mksqlite('select shortName,longName,units from channels');
+
+RSK.epochs = mksqlite('select deploymentID,startTime/1.0 as startTime, endTime/1.0 as endTime from epochs');
+RSK.epochs.startTime = RSKtime2datenum(RSK.epochs.startTime);
+RSK.epochs.endTime = RSKtime2datenum(RSK.epochs.endTime);
+
+RSK.schedules = mksqlite('select * from schedules');
+
+RSK.deployments = mksqlite('select * from deployments');
+
+RSK.instruments = mksqlite('select * from instruments');
 
 %% Remove non marine channels
 results = mksqlite('select isDerived from channels');
