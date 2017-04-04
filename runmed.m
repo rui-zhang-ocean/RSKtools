e4e13a0152d22fa145463194789429269a988723
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-04-04 14:29:50 -0400

    Add padding options

--- runmed.m
+++ runmed.m
@@ -1,18 +1,21 @@
-function [out, windowLength] = runmed(in, windowLength)
+function [out, windowLength] = runmed(in, windowLength, edgevalues)
 
 % runmed - Smooth a time series using a running median filter.
 %
-% Syntax:  [out, windowLength] = runmed(in, windowLength)
+% Syntax:  [out, windowLength] = runmed(in, windowLength, edgevalues)
 % 
 % runmed is helper function that performs a running median of length
-% windowLength. The time series is mirror padded to have values at the
-% edges.
+% windowLength. The time series is mirror padded, nan padded or zero
+% order hold padded
 %
 % Inputs:
 %    in - time series
 %
 %    windowLength - The length of the running median. It must be odd, has
 %        one added if it's found to be even. 
+%
+%    edgevalues - Describes how the filter will act at the edges. Options
+%         are 'mirrorpad', 'zeroOrderhold' and 'nanpad'.
 %
 % Outputs:
 %    out - the smoothed median time series
@@ -23,7 +26,7 @@ function [out, windowLength] = runmed(in, windowLength)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-03-22
+% Last revision: 2017-04-04
 
 n = length(in);
 out = NaN*in;
@@ -34,12 +37,18 @@ if mod(windowLength, 2) == 0
 end
 
 %% Mirror pad the time series
-padsize = (windowLength-1)/2;
-inpadded = mirrorpad(in, padsize);
+switch edgevalues
+    case 'mirrorpad'
+        inpadded = mirrorpad(in, padsize);
+    case 'nanpad'
+        inpadded = nanpad(in, padsize);
+    case 'zeroOrderhold'
+        inpadded = zeroOrderholdpad(in, padsize);
+end
 
 %% Running median
 for ndx = 1:n
-    out(ndx) = median(inpadded(ndx:ndx+(windowLength-1)));
+    out(ndx) = nanmedian(inpadded(ndx:ndx+(windowLength-1)));
 end
 
 end
