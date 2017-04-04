33cf52bfb2bb295d249615990b5c504f2e276535
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-04-04 16:52:44 -0400

    Inital commit for function that pads vector with zero order hold pre and post vector

---
+++ zeroOrderholdpad.m
@@ -0,0 +1,43 @@
+function out = zeroOrderholdpad(in, padSize)
+
+% zeroOrderholdpad - Pad a vector by entering Nan.
+%
+% Syntax:  out = zeroOrderholdpad(in, windowLength)
+% 
+% Pads vector with window length of entries at the beginning and end of the
+% in vector. The added entries are repeated first and last entry.
+%
+% Inputs:
+%    in - a vector
+%
+%    padSize - The length of the padding on each side of the vector.
+%         Must be <= length(in).
+%
+% Outputs:
+%    out - padded in vector of length length(in)+2*padSize
+%
+% Example: 
+%    out = zeroOrderholdpad([1:10], 3)
+% 
+%    out =
+%
+%    [1 1 1 1 2 3 4 5 6 7 8 9 10 10 10 10]
+%
+% Author: RBR Ltd. Ottawa ON, Canada
+% email: support@rbr-global.com
+% Website: www.rbr-global.com
+% Last revision: 2017-04-04
+
+
+if isrow(in)
+    pre = repmat(in(1), [1,padSize]);
+    post = repmat(in(end), [1,padSize]);
+    out = [pre in post];
+elseif iscolumn(in)
+    pre = repmat(in(1), [padSize,1]);
+    post = repmat(in(end), [padSize,1]);
+    out = [pre; in; post];
+else
+    error('in must be a row or column vector.')
+end
+end
