c25dcd7efc43d19a68084d8483e4f43fc7f6c504
Mark Halverson <mark.halverson@rbr-global.com>
2017-04-25 13:44:48 -0700

    Improvements to help text in header

--- shiftarray.m
+++ shiftarray.m
@@ -1,27 +1,27 @@
 function out = shiftarray(in, shift, edgepad)
 
 % shiftarray - Convenience function to shift a time series by a
-% specified number of samples
+% specified number of samples.
 %
 % Syntax:  out = shiftarray(in, shift, edgepad)
 % 
 % Shifts a vector time series by a lag corresponding to an integer
-% number of samples, e.g. for aligning temperature and conductivity
-% prior to the calculation of salinity. Negative shifts correspond to
-% moving the samples backwards in time (earlier), positive to forwards
-% in time (later). Values at either the beginning or the end are set
-% to the value of the original end point (zero order hold).
+% number of samples. Negative shifts correspond to moving the samples
+% backward in time (earlier), positive to forward in time (later). To
+% conserve the length of the output vector, values at either the
+% beginning or the end are set to a value specified by the argument
+% "edgepad."
 %
 % Inputs:
-%    in - The input time series
+%    in - The input time series.
 %
-%    shift - The number of samples to shift by
+%    shift - The number of samples to shift by.
 %
 %    edgepad - The values to set the beginning or end values. Options are
-%    mirror (default), zeroorderhold and nan.
+%    'mirror' (default), 'zeroorderhold', and 'nan'.
 %
 % Outputs:
-%    out - The shifted time series
+%    out - The shifted time series.
 %
 % Example: 
 %    shiftedValues = shiftarray(rsk.data.values(:,1), -3); % shift back by 3 samples
