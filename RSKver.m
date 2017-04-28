f35d4b47e2ee5b08d5b8708567c766852a089845
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-04-28 12:21:40 -0400

    Returns the most recent version of the RSK file

--- RSKver.m
+++ RSKver.m
@@ -1,4 +1,4 @@
-function [v, vsnMajor, vsnMinor, vsnPatch, RSK]  = RSKver(RSK)
+function [v, vsnMajor, vsnMinor, vsnPatch]  = RSKver(RSK)
 
 % RSKver - Returns the version of the RSK file.
 %
@@ -35,40 +35,6 @@ else
     vsnMajor = str2double(vsn{1}{1});
     vsnMinor = str2double(vsn{1}{2});
     vsnPatch = str2double(vsn{1}{3});
-end
-
-% Bug in RSK schema 1.13.0
-if  (vsnMajor == 1)&&(vsnMinor == 13)&&(vsnPatch == 0) && length(RSK.dbInfo)>1 && strcmpi(RSK.dbInfo(end).type,'full')
-    vsnMajorlast = 1;
-    vsnMinorlast = 13;
-    vsnPatchlast = 0;
-    for ndx = 1:length(RSK.dbInfo)-1
-        if ~strcmpi(RSK.dbInfo(ndx).type,'skinny')
-            v = RSK.dbInfo(ndx).version;
-            vsn = textscan(v,'%s','delimiter','.');
-            vsnMajor = str2double(vsn{1}{1});
-            vsnMinor = str2double(vsn{1}{2});
-            vsnPatch = str2double(vsn{1}{3});
-            if vsnMajor > vsnMajorlast || (vsnMajor == vsnMajorlast)&&(vsnMinor > vsnMinorlast) || (vsnMajor == vsnMajorlast)&&(vsnMinor == vsnMinorlast)&&(vsnPatch > vsnPatchlast)
-                    vsnMajorlast = vsnMajor;
-                    vsnMinorlast = vsnMinor;
-                    vsnPatchlast = vsnPatch;
-                    type = RSK.dbInfo(ndx).type;
-            end
-        end
-    end
-    v = [num2str(vsnMajorlast) '.' num2str(vsnMinorlast) '.' num2str(vsnPatchlast)];
-    vsnMajor = vsnMajorlast;
-    vsnMinor = vsnMinorlast;
-    vsnPatch = vsnPatchlast;
-    
-    % write fix to file
-    mksqlite('begin');
-    mksqlite(['INSERT INTO `dbInfo` VALUES ("' v '","' type '")']);
-    mksqlite('commit');
-    mksqlite('analyze');
-    
-    RSK.dbInfo = mksqlite('select version,type from dbInfo');
 end
 
 end
