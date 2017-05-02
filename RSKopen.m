1076d81d6b149f106b669c963f2075e23fd25f52
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-05-02 14:33:41 -0400

    date

--- RSKopen.m
+++ RSKopen.m
@@ -37,7 +37,7 @@ function [RSK, dbid] = RSKopen(fname)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-04-06
+% Last revision: 2017-05-01
 
 RSKconstants
 
