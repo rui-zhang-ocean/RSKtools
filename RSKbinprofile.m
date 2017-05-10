a237f4a0ab50a89d846fa47b532e3270dd6feef5
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-05-10 10:02:56 -0400

    Return samples per bin
    RSKTOOLS-41

--- RSKbinprofile.m
+++ RSKbinprofile.m
@@ -67,68 +67,88 @@ binBy = p.Results.binBy;
 binSize = p.Results.binSize;
 boundary = p.Results.boundary;
 
-
 %% Determine if the structure has downcasts and upcasts & set profileNum accordingly
 profileIdx = checkprofiles(RSK, profileNum, direction);
 castdir = [direction 'cast'];
 
-
+%% Find max profile length
+profilelength = 0;
 for ndx = profileIdx
+    if size(RSK.profiles.(castdir).data(ndx).tstamp,1) > profilelength
+        profilelength = size(RSK.profiles.(castdir).data(ndx).tstamp,1);
+    end
+end
+Y = NaN(profilelength, length(profileIdx));
 
-    % Find reference channel
+k=1;
+for ndx = profileIdx;
     if strcmpi(binBy, 'Time')
         ref = RSK.profiles.(castdir).data(ndx).tstamp;
+        Y(1:length(ref),k) = ref-ref(1);
     else
         chanCol = getchannelindex(RSK, binBy);
         ref = RSK.profiles.(castdir).data(ndx).values(:,chanCol);
+        Y(1:length(ref),k) = ref;
     end
+    k = k+1;
+end
 
-    % Set up binArray
-    binArray = [];
-    switch direction
-        case 'up'
-            if isempty(boundary)
-                bounds = [ceil(nanmax(ref)) floor(nanmin(ref))];
-            else
-                bounds = [boundary floor(nanmin(ref))];
-            end
-            for nregime = 1:length(bounds)-1
-                binArray = [binArray bounds(nregime):-binSize(nregime):bounds(nregime+1)];
-            end
+% Set up binArray
+binArray = [];
+switch direction
+    case 'up'
+        if isempty(boundary)
+            boundary = [max(nanmax(Y)) floor(min(nanmin(Y)))];
+        else
+            boundary = [boundary floor(min(nanmin(Y)))];
+        end
+        for nregime = 1:length(boundary)-1
+            binArray = [binArray boundary(nregime):-binSize(nregime):boundary(nregime+1)];
+        end
 
-        case 'down'
-            if isempty(boundary)      
-                bounds = [floor(nanmin(ref)) ceil(nanmax(ref))];
-            else
-                bounds = [boundary ceil(nanmax(ref))];
-            end
-            for nregime = 1:length(bounds)-1
-                binArray = [binArray bounds(nregime):binSize(nregime):bounds(nregime+1)];       
-            end
-    end
-    binArray = unique(binArray);
+    case 'down'
+        if isempty(boundary)
+            boundary = [floor(min(nanmin(Y))) max(nanmax(Y))];
+        else
+            boundary = [boundary ceil(max(nanmax(Y)))];
+        end
+        for nregime = 1:length(boundary)-1
+            binArray = [binArray boundary(nregime):binSize(nregime):boundary(nregime+1)];       
+        end
+end
+binArray = unique(binArray);
 
-    % Binning 
+for ndx = profileIdx
+    % Binning
     X = [RSK.profiles.(castdir).data(ndx).tstamp, RSK.profiles.(castdir).data(ndx).values];
     binnedValues = NaN(length(binArray)-1, size(X,2));
     binCenter = tsmovavg(binArray, 's', 2);
     binCenter = binCenter(2:end); %Starts with NaN.
-    %  initialize the binned output field      
+    samplesinbin = NaN(length(binCenter),1);
+    %  initialize the binned output field
     for k=1:length(binArray)-1
-        kk = ref >= binArray(k) & ref < binArray(k+1);
-        if ~isempty(find(diff(kk)<0,1))
-           kk(ind(1):end) = 0;
+        kk = Y(:,ndx) >= binArray(k) & Y(:,ndx) < binArray(k+1);
+        ind = find(diff(kk)<0,1);
+        if ~isempty(ind)
+           kk(ind:end) = 0;
         end
+        samplesinbin(k) = sum(kk);
         binnedValues(k,:) = nanmean(X(kk,:),1);
     end
     
     RSK.profiles.(castdir).data(ndx).values = binnedValues(:,2:end);
+    RSK.profiles.(castdir).data(ndx).samplesinbin = samplesinbin;
     if strcmpi(binBy, 'Time')
         RSK.profiles.(castdir).data(ndx).tstamp = binCenter;
     else
         RSK.profiles.(castdir).data(ndx).tstamp = binnedValues(:,1);
         RSK.profiles.(castdir).data(ndx).values(:,chanCol) = binCenter;
     end
-    
+
 end
+
+unit = RSK.channels(chanCol).units;
+logprofile = logentryprofiles(direction, profileNum, profileIdx);
+logentry = sprintf('Binned with respect to %s using [%s] boundaries with %s %s bin size on %s.', binBy, num2str(boundary), num2str(binSize), unit, logprofile);
+RSK = RSKappendtolog(RSK, logentry);
 end
