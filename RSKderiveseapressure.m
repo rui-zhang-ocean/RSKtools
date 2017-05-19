8e5b47440adddf6cf0b531b53264b4153287fe51
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-05-19 14:47:09 -0400

    Adapt to new structure
    RSKTOOLS-81

--- RSKderiveseapressure.m
+++ RSKderiveseapressure.m
@@ -1,4 +1,4 @@
-function [RSK, salinity] = RSKderiveseapressure(RSK, varargin)
+function [RSK, seapressure] = RSKderiveseapressure(RSK, varargin)
 
 % RSKderiveseapressure - Calculate sea pressur and add it or replace it in the data table
 %
@@ -10,14 +10,7 @@ function [RSK, salinity] = RSKderiveseapressure(RSK, varargin)
 % column. 
 %
 % Inputs: 
-%    [Required] - RSK - Structure containing the logger metadata and data
-%
-%               
-%    [Optional] - series - Specifies the series to be filtered. Either 'data'
-%                     or 'profile'. Default is 'data'.
-%            
-%                 direction - 'up' for upcast, 'down' for downcast, or 'both' for
-%                     all. Default is 'down'.
+%    RSK - Structure containing the logger metadata and data
 %
 % Outputs:
 %    RSK - RSK structure containing the salinity data
@@ -25,33 +18,7 @@ function [RSK, salinity] = RSKderiveseapressure(RSK, varargin)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-05-12
-
-validSeries = {'profile', 'data'};
-checkSeriesName = @(x) any(validatestring(x,validSeries));
-
-validDirections = {'down', 'up', 'both'};
-checkDirection = @(x) any(validatestring(x,validDirections));
-
-p = inputParser;
-addRequired(p, 'RSK', @isstruct);
-addParameter(p, 'series', 'data', checkSeriesName)
-addParameter(p, 'direction', 'down', checkDirection);
-parse(p, RSK, varargin{:})
-
-RSK = p.Results.RSK;
-series = p.Results.series;
-direction = p.Results.direction;
-
-if strcmpi(series, 'profile')
-    if strcmpi(direction, 'both')
-        direction = {'down', 'up'};
-    else
-        direction = {direction};
-    end
-end
-
-%% Check TEOS-10 and CTP data are available.
+% Last revision: 2017-05-19
 
 Pcol = getchannelindex(RSK, 'Pressure');
 
@@ -59,25 +26,14 @@ Pcol = getchannelindex(RSK, 'Pressure');
 RSK = addchannelmetadata(RSK, 'Sea Pressure', 'dbar');
 SPcol = getchannelindex(RSK, 'Sea Pressure');
 
-switch series
-    case 'data'
-        data = RSK.data;
-        seapressure = data.values(:, Pcol)- 10.1325;
-        RSK.data.values(:,SPcol) = seapressure;
-    case 'profile'
-        for dir = direction
-            profileNum = [];
-            profileIdx = checkprofiles(RSK, profileNum, dir{1});
-            castdir = [dir{1} 'cast'];
-            for ndx = profileIdx
-                data = RSK.profiles.(castdir).data(ndx);
-                seapressure = data.values(:, Pcol)- 10.1325;
-                RSK.profiles.(castdir).data(ndx).values(:,SPcol) = seapressure;
-            end
-        end
+
+dataIdx = setdataindex(RSK);
+for ndx = dataIdx
+    seapressure = RSK.data(ndx).values(:, Pcol)- 10.1325;
+    RSK.data(ndx).values(:,SPcol) = seapressure;
 end
 
 end
-
+ %fix me: seapressure is the last one calculate only.
 
 
