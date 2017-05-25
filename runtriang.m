6cad9b456b3a99cd6a2b694849c7f6f5fb4a64cb
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-05-25 16:44:34 -0400

    Inital commit for function to do a running triangle filter
    RSKTOOLS-44 #comment I am starting to work on a triangle filter for RSKsmooth.m it uses runtriang.m

---
+++ runtriang.m
@@ -0,0 +1,65 @@
+function [out] = runtriang(in, windowLength, edgepad)
+
+% runtriang - Smooth a time series using a triangle filter.
+%
+% Syntax:  [out] = runtriang(in, windowLength, edgepad)
+% 
+% runtriang performs a triangle filter, of length windowLength over the time
+% series. 
+%
+% Inputs:
+%    in - time series
+%
+%    windowLength - The length of the running triangle. It must be odd,
+%         will add one if it is odd.
+%
+%    edgepad - Describes how the filter will act at the edges. Options
+%         are 'mirror', 'zeroorderhold' and 'nan'.
+%
+% Outputs:
+%    out - the smoothed time series
+%
+% Author: RBR Ltd. Ottawa ON, Canada
+% email: support@rbr-global.com
+% Website: www.rbr-global.com
+% Last revision: 2017-05-25
+
+%% Check and set inputs/outputs
+if nargin == 2
+    edgepad = 'mirror';
+end
+
+n = length(in);
+out = NaN*in;
+
+
+%% Check windowLength
+if mod(windowLength, 2) == 0
+    error('windowLength must be odd');
+end
+
+padsize = (windowLength-1)/2;
+%% Mirror pad the time series
+switch edgepad
+    case 'mirror'
+        inpadded = mirrorpad(in, padsize);
+    case 'nan'
+        inpadded = nanpad(in, padsize);
+    case 'zeroorderhold'
+        inpadded = zeroorderholdpad(in, padsize);
+end
+
+for ndx = 1:windowLength
+    if ndx <= (windowLength+1)/2
+        coeff(ndx) = 2*ndx/(windowLength+1);
+    else
+        coeff(ndx) = 2 - (2*ndx/(windowLength+1));
+    end
+end
+normcoeff = (coeff/sum(coeff));
+
+for ndx = 1:n
+    out(ndx) = nansum(inpadded(ndx:ndx+(windowLength-1)).*normcoeff);
+end
+
+end
