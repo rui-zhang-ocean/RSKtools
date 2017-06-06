4b9a59fd518d072923119da9b3e58adcc092f29d
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-06 13:47:05 -0400

    Output geodata in matrix format

--- RSKreadgeodata.m
+++ RSKreadgeodata.m
@@ -1,14 +1,15 @@
-function RSK = RSKreadgeodata(RSK, varargin)
+function [RSK, geodata] = RSKreadgeodata(RSK, varargin)
 
 % RSKreadgeodata - Reads the geodata of a rsk file
 %
-% Syntax:  RSK = RSKreadgeodata(RSK)
+% Syntax:  [RSK, geodata] = RSKreadgeodata(RSK)
 %
 % RSKreadgeodata will return the geodata of a file
 %
 % Inputs:
 %    RSK - Structure containing the logger metadata and thumbnails
 %          returned by RSKopen.
+%
 %    UTCdelta - The offset of the timestamp. If a value is entered it will
 %          be used. Otherwise it will use the one given in the epochs
 %          table, if there is none it will use 0.
@@ -17,24 +18,23 @@ function RSK = RSKreadgeodata(RSK, varargin)
 %    RSK - Structure containing previously present logger metadata as well
 %          as geodata.
 %
+%    geodata - The contents of the geodata field returned as an array.
+%
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-01-18
+% Last revision: 2017-06-02
 
-%% Parse Inputs
 p = inputParser;
 addRequired(p, 'RSK', @isstruct);
 addParameter(p, 'UTCdelta', 0);
 parse(p, RSK, varargin{:})
 
-% Assign each input argument
 RSK = p.Results.RSK;
 UTCdelta = p.Results.UTCdelta;
 
 
 
-%% Read geodata
 RSK.geodata = mksqlite('select tstamp/1.0 as tstamp, latitude, longitude, accuracy, accuracyType from geodata');
 if isempty(RSK.geodata)
     RSK = rmfield(RSK, 'geodata');
@@ -53,5 +53,6 @@ for ndx = 1:length(RSK.geodata)
     RSK.geodata(ndx).tstamp = RSKtime2datenum(RSK.geodata(ndx).tstamp + UTCdelta);
 end
 
+geodata = [[RSK.geodata.tstamp]', [RSK.geodata.latitude]', [RSK.geodata.longitude]', [RSK.geodata.accuracy]'];
 
 end
