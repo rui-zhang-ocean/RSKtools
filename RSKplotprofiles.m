06b39afc7b76ded4bad323dd243bbc9477b4ed64
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-20 10:28:58 -0400

    Fix channel argument as cell array

--- RSKplotprofiles.m
+++ RSKplotprofiles.m
@@ -1,14 +1,12 @@
 function handles = RSKplotprofiles(RSK, varargin)
 
-% RSKplotprofiles - Plot profiles from an RSK structure output by 
-%                   RSKreadprofiles.
+% RSKplotprofiles - Plot summaries of logger data as profiles.
 %
 % Syntax:  [handles] = RSKplotprofiles(RSK, [OPTIONS])
 % 
 % Plots profiles from automatically detected casts. If called with one
-% argument, will default to plotting downcasts of temperature for all
-% elements in the data field.  Optionally outputs an array of handles
-% to the line objects.
+% argument, will default to plotting dowall the casts of all channels
+% available. Optionally outputs an array of handles to the line objects. 
 %
 % Inputs: 
 %    [Required] - RSK - Structure containing the logger metadata and data
@@ -16,28 +14,28 @@ function handles = RSKplotprofiles(RSK, varargin)
 %    [Optional] - profile - Optional profile number to plot. Default is to plot 
 %                        all detected profiles.
 %
-%                 channel - Variable to plot (e.g. temperature, salinity,
-%                        etc). Default is 'Temperature'.
+%                 channel - Variables to plot (e.g. temperature, salinity,
+%                        etc). Default is all.
 % 
 %                 direction - 'up' for upcast, 'down' for downcast or
-%                        'both'. Default is 'down'.
+%                        'both'.
 %
 % Output:
 %     handles - The line object of the plot.
 %
 % Examples:
 %    rsk = RSKopen('profiles.rsk');
-%    rsk = RSKreadprofiles(rsk);
+%    rsk = RSKreadprofiles(rsk, 'direction', 'down');
 %    % plot selective downcasts and output handles
 %      for customization
-%    hdls = RSKplotprofiles(rsk, 'profile', [1 5 10], 'channel', 'conductivity');
+%    hdls = RSKplotprofiles(rsk, 'profile', [1 5 10], 'channel', {'Conductivity', 'Temperature'});
 %
 % See also: RSKreadprofiles, RSKreaddata.
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-06-19
+% Last revision: 2017-06-20
 
 validDirections = {'down', 'up', 'both'};
 checkDirection = @(x) any(validatestring(x,validDirections));
@@ -45,8 +43,8 @@ checkDirection = @(x) any(validatestring(x,validDirections));
 p = inputParser;
 addRequired(p, 'RSK', @isstruct);
 addParameter(p, 'profile', [], @isnumeric);
-addParameter(p, 'channel', 'all', @ischar)
-addParameter(p, 'direction', 'down', checkDirection);
+addParameter(p, 'channel', 'all')
+addParameter(p, 'direction', [], checkDirection);
 parse(p, RSK, varargin{:})
 
 % Assign each input argument
@@ -61,7 +59,11 @@ direction = p.Results.direction;
 if strcmp(channel, 'all')
     chanCol = 1:length({RSK.channels.longName});
 else
-    chanCol = getchannelindex(RSK, channel);
+    chanCol = [];
+    channels = cellchannelnames(RSK, channel);
+    for chan = channels
+        chanCol = [chanCol getchannelindex(RSK, chan{1})];
+    end
 end
 numchannels = length(chanCol);
 castidx = getdataindex(RSK, profile, direction);
@@ -84,13 +86,14 @@ for chan = chanCol
     xlabel(RSK.channels(chan).units);
     ylabel('Sea pressure [dbar]')
     set(gca, 'ydir', 'reverse')
-    ax(n)=gca;
+    ax(n) = gca;
     ax(n).ColorOrderIndex = 1; 
     n = n+1 ;
     grid
+    hold off
 end
 linkaxes(ax,'y')
 shg
-hold off
+
 
 end
