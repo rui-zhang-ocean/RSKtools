335353a16d1208683d84d9f5a8d0a3f320ebdd50
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-20 15:40:18 -0400

    Improve documentation and simplify function

--- getseapressure.m
+++ getseapressure.m
@@ -1,28 +1,28 @@
-function [RSKsp, SPcol] = getseapressure(RSK)
+function [RSK, SPcol] = getseapressure(RSK)
 
-% getseapressure - Checks if the RSK has sea pressure. If not it derives
-% it, output the RSK structure with sea pressure and the column index.
+%GETSEAPRESSURE - Add sea pressure to RSK and return the column index.
 %
-% Syntax:  [RSKsp, SPcol] = getseapressure(RSK)
-
+% Syntax:  [RSK, SPcol] = getseapressure(RSK)
+%
+% Finds the column index of sea pressure if it exists, if not it derives
+% sea pressure and adds it to the RSK structure.
 %
 % Inputs: 
-%    RSK - Structure containing the logger metadata and data
+%    RSK - Structure containing the logger metadata and data.
 %
 % Outputs:
-%    RSKsp - RSK structure containing the sea pressure data
+%    RSK - RSK structure containing the sea pressure data
 %
 %    SPcol - Channel index for sea pressure.
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-05-19
+% Last revision: 2017-06-20
 
 try
     SPcol = getchannelindex(RSK, 'Sea Pressure');
-    RSKsp = RSK;
 catch
-    RSKsp = RSKderiveseapressure(RSK);
-    SPcol = getchannelindex(RSKsp, 'Sea Pressure');
+    RSK = RSKderiveseapressure(RSK);
+    SPcol = getchannelindex(RSK, 'Sea Pressure');
 end
