a7ebcb7ad50e5b42ae9d29df30c53abe6ffcaab9
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-20 09:57:20 -0400

    Add tstamp entry with now time with each entry
    RSKTOOLS-57 #comment A 'now' timestamp is added with each entry.

--- RSKappendtolog.m
+++ RSKappendtolog.m
@@ -1,27 +1,26 @@
 function RSK = RSKappendtolog(RSK, logentry)
 
-% RSKappendtolog - Appends the a string to the log field in RSK structure
+% RSKappendtolog - Appends the entry and current time to the log field in
+% RSK structure 
 %
-% Syntax:  [RSK] = RSKappendtolog(RSK, logstring)
+% Syntax:  [RSK] = RSKappendtolog(RSK, logentry)
 % 
-% RSKappendtolog adds an entry to the log field in the RSK structure. If
-% the field isn't present it creates it. It only ever appends entries to
-% the end.
+% Adds an entry and the current time to the log field in the RSK structure.
+% If the field isn't present it creates it. It only ever appends entries to
+% the end. 
 %
 % Inputs: 
 %    RSK - Structure containing the logger metadata and thumbnails
 %
-%    logentry - Comment that will be added to the log. Must be string
-%        entry.
+%    logentry - Comment that will be added to the log. Must be a string. 
 %
 % Outputs:
 %    RSK - The RSK structure with updated log field
 %
-%
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-03-16
+% Last revision: 2017-06-20
 
 if isfield(RSK, 'log')
     nlog = length(RSK.log);
@@ -29,4 +28,4 @@ else
     nlog = 0;
 end
 
-RSK.log(nlog+1,1) = {logentry};
+RSK.log(nlog+1,:) = {now, logentry};
