ac1bc75d151ba3ca66086f932d6cfed2dbacc751
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-21 17:38:55 -0400

    Improve documentation

--- RSKappendtolog.m
+++ RSKappendtolog.m
@@ -1,26 +1,27 @@
 function RSK = RSKappendtolog(RSK, logentry)
 
-% RSKappendtolog - Appends the entry and current time to the log field in
-% RSK structure 
+%RSKappendtolog - Append the entry and current time to the log field.
 %
 % Syntax:  [RSK] = RSKappendtolog(RSK, logentry)
 % 
-% Adds an entry and the current time to the log field in the RSK structure.
+% Adds the current time and logentry  to the log field in the RSK structure.
 % If the field isn't present it creates it. It only ever appends entries to
 % the end. 
 %
 % Inputs: 
-%    RSK - Structure containing the logger metadata and thumbnails
+%    RSK - Structure containing the logger metadata and thumbnail
 %
 %    logentry - Comment that will be added to the log. Must be a string. 
 %
 % Outputs:
-%    RSK - The RSK structure with updated log field
+%    RSK - Input structure with updated log field.
+%
+% See also: RSKopen, RSKalignchannel, RSKbinaverage.
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-06-20
+% Last revision: 2017-06-21
 
 if isfield(RSK, 'log')
     nlog = length(RSK.log);
@@ -29,3 +30,5 @@ else
 end
 
 RSK.log(nlog+1,:) = {now, logentry};
+
+end
