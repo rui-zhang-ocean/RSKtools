3aa259a152aee14f80ac5d719af679e2bab31752
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-06-23 09:58:52 -0400

    doocs

--- RSKselectdowncast.m
+++ RSKselectdowncast.m
@@ -1,34 +1,38 @@
 function [RSK, downidx] = RSKselectdowncast(RSK)
 
-% RSKselectdowncast - Selects the datafields that have a increasing pressure.
+%RSKselectdowncast - Keep the data elements that have a increasing pressure.
 %
 % Syntax:  [RSK, isDown] = RSKselectdowncast(RSK)
 %
-% This function only keeps the downcasts in the RSK and returns the index of
-% the downcasts from the input RSK structure.
+% Keeps the downcasts in the RSK and returns the index of the downcasts
+% from the input RSK structure. 
 %
 % Inputs:
-%    RSK - The input RSK structure
+%    RSK - Structure containing logger data.
 %
 % Outputs:
-%    RSK - The RSK structure only containing upcasts.
+%    RSK - Structure only containing downcast data.
 %
-%    downidx - The index of the data fields from the input RSK structure
-%          are downcasts.
+%    downidx - Index of the data fields from the input RSK structure that
+%          are downcasts. 
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-05-25
+% Last revision: 2017-06-23
 
 Pcol = getchannelindex(RSK, 'Pressure');
 ndata = length(RSK.data);
+
+
 
 downidx = NaN(1, ndata);
 for ndx = 1:ndata
     pressure = RSK.data(ndx).values(:, Pcol);
     downidx(1, ndx) = getcastdirection(pressure, 'down');
 end
+
+
 
 RSK.profiles.originalindex = RSK.profiles.originalindex(logical(downidx));
 RSK.data = RSK.data(logical(downidx));
--- RSKselectupcast.m
+++ RSKselectupcast.m
@@ -1,34 +1,38 @@
 function [RSK, upidx] = RSKselectupcast(RSK)
 
-% RSKselectupcast - Selects the datafields that have a decreasing pressure.
+%RSKselectupcast - Select the data elements that have decreasing pressure.
 %
 % Syntax:  [RSKup, isUp] = RSKselectupcast(RSK)
 %
-% This function only keeps the upcasts in the RSK and returns the index of
-% the upcasts from the input RSK structure.
+% TKeps the upcasts in the RSK and returns the index of the upcasts from
+% the input RSK structure. 
 %
 % Inputs:
-%    RSK - The input RSK structure
+%    RSK - Structure containing logger data.
 %
 % Outputs:
-%    RSK - The RSK structure only containing upcasts.
+%    RSK - Structure only containing upcasts.
 %
-%    upidx - The index of the data fields from the input RSK structure
+%    upidx - Index of the data fields from the input RSK structure that 
 %          are upcasts.
 %
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-05-25
+% Last revision: 2017-06-23
 
 Pcol = getchannelindex(RSK, 'Pressure');
 ndata = length(RSK.data);
+
+
 
 upidx = NaN(1, ndata);
 for ndx = 1:ndata
     pressure = RSK.data(ndx).values(:, Pcol);
     upidx(1, ndx) = getcastdirection(pressure, 'up');
 end
+
+
 
 RSK.profiles.originalindex = RSK.profiles.originalindex(logical(upidx));
 RSK.data = RSK.data(logical(upidx));
