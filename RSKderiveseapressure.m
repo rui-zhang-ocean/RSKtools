6abc68c2504eedcbc14e854a58bf5b845689cb3c
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-07-04 10:03:10 -0400

    Add patm as a input parameter

--- RSKderiveseapressure.m
+++ RSKderiveseapressure.m
@@ -9,7 +9,10 @@ function [RSK] = RSKderiveseapressure(RSK, varargin)
 % overwrites that data column.  
 %
 % Inputs: 
-%    RSK - Structure containing the logger metadata and data.
+%    [Required] - RSK - Structure containing the logger metadata and data
+%
+%    [Optional] - pAtm - Atmospheric Pressure. Default is value stored in
+%                       parameters table or 10.1325 dbar if unavailable. 
 %
 % Outputs:
 %    RSK - Structure containing the salinity data.
@@ -19,9 +22,20 @@ function [RSK] = RSKderiveseapressure(RSK, varargin)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-06-22
+% Last revision: 2017-07-04
 
-pAtm = getatmosphericpressure(RSK);
+p = inputParser;
+addRequired(p, 'RSK', @isstruct);
+addParameter(p, 'pAtm', [], @isnumeric);
+parse(p, RSK, varargin{:})
+
+RSK = p.Results.RSK;
+pAtm = p.Results.pAtm;
+
+if isempty(pAtm)
+    pAtm = getatmosphericpressure(RSK);
+end
+
 Pcol = getchannelindex(RSK, 'Pressure');
 
 
