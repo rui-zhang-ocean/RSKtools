3349a3cae02d521ea663ebbc535f6b50d1b50c0f
Muriel Dunn <muriel.dunn@rbr-global.com>
2017-07-10 10:54:28 -0400

    Enumerate channels with duplicate longName
    RSKTOOLS-55

--- renamechannels.m
+++ renamechannels.m
@@ -6,7 +6,7 @@ function RSK = renamechannels(RSK)
 %
 % Checks for shortNames that correspond to channels that require a more
 % descriptive name and replaces the longName. These are doxy, temp04,
-% temp05, temp10, temp13 and pres08. 
+% temp05, temp10, temp13 and pres08. Enumerates duplicate longNames.
 % 
 % Inputs:
 %    RSK - Structure containing metadata.
@@ -19,20 +19,14 @@ function RSK = renamechannels(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-07-04
+% Last revision: 2017-07-10
 
 shortName = {RSK.channels.shortName};
-
-idx = find(strcmpi(shortName, 'temp04'));
-for ndx = 1:length(idx)
-    RSK.channels(idx(ndx)).longName = ['Temperature' num2str(ndx)];
-end
 
 idx = (strcmpi(shortName, 'temp05') | strcmpi(shortName, 'temp10'));
 if any(idx)
     RSK.channels(idx).longName = 'Pressure Gauge Temperature';
 end
-
 
 idx = strcmpi(shortName, 'temp13');
 if any(idx)
@@ -48,4 +42,21 @@ idx = strcmpi(shortName, 'pres08');
 if any(idx)
     RSK.channels(idx).longName = 'Sea Pressure';
 end
+
+
+
+% Number duplicate longnames.
+longname = {RSK.channels.longName};
+[~, ~, channameidx] = unique(longname, 'stable');
+idx = find(hist(channameidx, unique(channameidx))>1);
+
+for ndup = 1:length(idx)
+    k = 1;
+    duplicates = find(channameidx==idx(ndup));
+    for ndx = duplicates'
+        RSK.channels(ndx).longName = [RSK.channels(ndx).longName '' num2str(k)];
+        k = k+1;
+    end
+end
+
 end
