c9b8f71fbf4279bd1086d778cb71874035a7b1d7
Rui Zhang <rui.zhang@rbr-global.com>
2017-10-03 08:43:42 -0400

    add different linestyle for upcast and downcast with same color

--- RSKplotprofiles.m
+++ RSKplotprofiles.m
@@ -4,10 +4,10 @@ function handles = RSKplotprofiles(RSK, varargin)
 %
 % Syntax:  [handles] = RSKplotprofiles(RSK, [OPTIONS])
 % 
-% Plots profiles from automatically detected casts. The default is to
-% plot all channels (excluding pressure, sea pressure and depth) for
-% all casts against sea pressure or depth.  Optionally outputs a
-% matrix of handles to the line objects.
+% Plots profiles from automatically detected casts. The default is to plot
+% all the casts of all channels available (excluding Pressure, Sea Pressure
+% and Depth) against sea pressure (options to plot against depth).
+% Optionally outputs a matrix of handles to the line objects.
 %
 % Inputs: 
 %    [Required] - RSK - Structure containing the logger metadata and data.
@@ -21,10 +21,12 @@ function handles = RSKplotprofiles(RSK, varargin)
 % 
 %                 direction - 'up' for upcast, 'down' for downcast or
 %                        'both'. Default is to use all directions
-%                        available.
+%                        available. When choosing 'both', downcast with
+%                        solid while upcast with dashed line.
 % 
 %                 reference - Channel plotted on the y axis for each
-%                        subplot.
+%                        subplot. Default is sea pressure, option for
+%                        depth.
 %
 % Output:
 %     handles - Line object of the plot.
@@ -40,12 +42,12 @@ function handles = RSKplotprofiles(RSK, varargin)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-08-30
+% Last revision: 2017-10-03
 
 validDirections = {'down', 'up', 'both'};
 checkDirection = @(x) any(validatestring(x,validDirections));
 
-validReference = {'sea pressure', 'depth'};
+validReference = {'Sea Pressure', 'Depth'};
 checkReference = @(x) any(validatestring(x,validReference));
 
 p = inputParser;
@@ -53,7 +55,7 @@ addRequired(p, 'RSK', @isstruct);
 addParameter(p, 'profile', [], @isnumeric);
 addParameter(p, 'channel', 'all')
 addParameter(p, 'direction', [], checkDirection);
-addParameter(p, 'reference', 'sea pressure', checkReference)
+addParameter(p, 'reference', 'Sea Pressure', checkReference)
 parse(p, RSK, varargin{:})
 
 RSK = p.Results.RSK;
@@ -74,51 +76,62 @@ end
 numchannels = length(chanCol);
 
 castidx = getdataindex(RSK, profile, direction);
-if strcmpi(reference, 'depth')
-    ycol = getchannelindex(RSK, 'depth');
+if strcmpi(reference, 'Depth')
+    ycol = getchannelindex(RSK, 'Depth');
     RSKy = RSK;
 else
     [RSKy, ycol] = getseapressure(RSK);
 end
 
-% In R2014a and earlier, lines plotted after calling 'hold on' are
-% drawn in the same color as the first.  To overcome this here, line
-% colours are specified manually.  Default behaviour in R2014b and
+% In 2014a and earlier, lines plotted after calling 'hold on' results
+% in are the same color as the original.  To overcome this, specify
+% colors manually to overcome this behaviour.  Default in 2014b and
 % later is to use the next color in axescolororder, but we proceed
-% with the following fix anyway because it is compatible with R2014b
+% with the following fix anyway because it is compatible with 2014b
 % and later.
 clrs = get(0,'defaultaxescolororder');
 ncast = length(castidx); % up and down are both casts
-clrs = repmat(clrs,ceil(ncast/7),1); % 7 separate colours in default color order
+clrs = repmat(clrs,ceil(ncast/7),1);
 clrs = clrs(1:ncast,:);
 
 
-pmin = [];
-pmax = [];
+
+pmax = 0;
 n = 1;
 for chan = chanCol
     subplot(1,numchannels,n)
-    ii = 1;
-    for ndx = castidx
-        ydata = RSKy.data(ndx).values(:, ycol);
-        handles(ii,n) = plot(RSK.data(ndx).values(:, chan), ydata,'color',clrs(ii,:));
-        hold on
-        pmin = min([pmin; ydata]);
-        pmax = max([pmax; ydata]);
-        ii = ii+1;
+    
+    if strcmp(direction, 'both') % downcast in solid and upcast in dashed line with the same color
+        ii = 1;
+        for ndx = castidx(1:2:end) 
+            ydata = RSKy.data(ndx).values(:, ycol);
+            handles(ii,n) = plot(RSK.data(ndx).values(:, chan), ydata,'color',clrs(ii,:));
+            hold on
+            ydata = RSKy.data(ndx+1).values(:, ycol);
+            handles(ii,n) = plot(RSK.data(ndx+1).values(:, chan), ydata,'color',clrs(ii,:),'linestyle','--');
+            pmax = max([pmax; ydata]);
+            ii = ii+1;
+        end
+    else  % all solid line if only downcast or upcast is chosen
+        ii = 1;
+        for ndx = castidx 
+            ydata = RSKy.data(ndx).values(:, ycol);
+            handles(ii,n) = plot(RSK.data(ndx).values(:, chan), ydata,'color',clrs(ii,:));
+            hold on
+            pmax = max([pmax; ydata]);
+            ii = ii+1;
+        end
     end
+    
+    ylim([0 pmax])
     title(RSK.channels(chan).longName);
     xlabel(RSK.channels(chan).units);
     ylabel([RSKy.channels(ycol).longName ' [' RSKy.channels(ycol).units ']'])
     n = n+1;
     grid on
 end
-
-% set some y-axis properties
-
-ax = findobj(gcf,'type','axes');
-set(ax, 'ydir', 'reverse', 'ylim', [pmin pmax])
-
+ax = findall(gcf,'type','axes');
+set(ax, 'ydir', 'reverse')
 linkaxes(ax,'y')
 shg
 
