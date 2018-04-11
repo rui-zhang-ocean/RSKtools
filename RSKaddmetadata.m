9a874f2bd17f05d564d770d2d5a20c7d46e7eae1
Mark Halverson <mark.halverson@rbr-global.com>
2018-04-11 15:48:21 -0700

    Added 'station' as metadata field

--- RSKaddmetadata.m
+++ RSKaddmetadata.m
@@ -4,37 +4,40 @@ function [RSK] = RSKaddmetadata(RSK, profile, varargin)
 %
 % Syntax:  [RSK] = RSKaddmetadata(RSK, profile, [OPTIONS])
 % 
-% Append metadata to data structure with profiles, including latitude,
-% longitude, comment and description. The function is vectorized, which 
-% allows multiple metadata inputs for multiple profiles. But when there is
-% only one metadata input for multiple profiles, all pointed profiles will
-% be assigned with the same value.
+% Append station metadata to data structure with profiles, including
+% latitude, longitude, station name, comment, and description. The
+% function is vectorized, which allows multiple metadata inputs for
+% multiple profiles. But when there is only one metadata input for
+% multiple profiles, all pointed profiles will be assigned with the
+% same value.
 %
 % Inputs: 
 %   [Required] - RSK - Structure containing data. 
 %    
-%                profile - Profile number to have metadata add on.
+%                profile - Profile number(s) to which metadata should be assigned.
 % 
 %   [Optional] - lat - Profile latitude coordinate
 %
 %                lon - Profile longitude coordinate
 %
+%                station - Cell array of strings with station name(s) 
+%                 
 %                comment - Comment for specified profile
 %
 %                description - Decription for specified profile
 %
 % Outputs:
-%    RSK - Updated structure containing metadata for specified profile.
+%    RSK - Updated structure containing metadata for specified profile(s).
 %
 % Example:
-%    RSK = RSKaddmetadata(RSK, 4,'lat',45,'lon',-25,'comment',{'NoComment'},'description',{'Cruise in North Atlantic with ..'})
+%    RSK = RSKaddmetadata(RSK, 4,'lat',45,'lon',-25,'station',{'NA1'},'comment',{'NoComment'},'description',{'Cruise in North Atlantic with ..'})
 %    OR
 %    RSK = RSKaddmetadata(RSK, 4:6,'lat',[45,44,46],'lon',[-25,-24,-23],'comment',{'Comment1','Comment2','Comment3'});
 % 
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2018-03-07
+% Last revision: 2018-04-11
 
 
 p = inputParser;
@@ -42,6 +45,7 @@ addRequired(p, 'RSK', @isstruct);
 addRequired(p, 'profile', @isnumeric);
 addParameter(p, 'lat', [], @isnumeric);
 addParameter(p, 'lon', [], @isnumeric);
+addParameter(p, 'station', '', @iscell);
 addParameter(p, 'comment', '', @iscell);
 addParameter(p, 'description', '', @iscell);
 parse(p, RSK, profile, varargin{:})
@@ -50,6 +54,7 @@ RSK = p.Results.RSK;
 profile = p.Results.profile;
 lat = p.Results.lat;
 lon = p.Results.lon;
+station = p.Results.station;
 comment = p.Results.comment;
 description = p.Results.description;
 
@@ -66,6 +71,7 @@ k = 1;
 for i = 1:directions:length(castidx);    
     RSK = assign_metadata(RSK, lat, castidx, i, directions, profile, k, 'latitude');
     RSK = assign_metadata(RSK, lon, castidx, i, directions, profile, k, 'longitude');
+    RSK = assign_metadata(RSK, station, castidx, i, directions, profile, k, 'station');
     RSK = assign_metadata(RSK, comment, castidx, i, directions, profile, k, 'comment');
     RSK = assign_metadata(RSK, description, castidx, i, directions, profile, k, 'description');
     k = k + 1;    
