2597070ab4cac8780e47ea23953ede084ac95088
Rui Zhang <rui.zhang@rbr-global.com>
2018-05-28 15:35:18 -0400

    RSKopen reads all columns for region table

--- RSKreadannotations.m
+++ RSKreadannotations.m
@@ -24,12 +24,6 @@ function RSK = RSKreadannotations(RSK)
 tables = doSelect(RSK, 'SELECT name FROM sqlite_master WHERE type="table"');
 
 if any(strcmpi({tables.name}, 'region')) && any(strcmpi({tables.name}, 'regionGeoData')) && any(strcmpi({tables.name}, 'regionComment'))
-    regioninfo = doSelect(RSK, 'PRAGMA table_info(region)');
-    if any(strcmpi({regioninfo.name}, 'description')) % description column only exists after 1.13.8 
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2, description from region');
-    else
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2 from region');
-    end
     RSK.regionGeoData = doSelect(RSK, 'select * from regionGeoData');
     RSK.regionComment = doSelect(RSK, 'select * from regionComment');
 else
--- readregionprofiles.m
+++ readregionprofiles.m
@@ -18,17 +18,12 @@ function RSK = readregionprofiles(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2018-02-09
+% Last revision: 2018-05-28
 
 tables = doSelect(RSK, 'SELECT name FROM sqlite_master WHERE type="table"');
 
 if any(strcmpi({tables.name}, 'regionCast')) && any(strcmpi({tables.name}, 'region'))
-    regioninfo = doSelect(RSK, 'PRAGMA table_info(region)');
-    if any(strcmpi({regioninfo.name}, 'description')) % description column only exists after 1.13.8 
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2, description from region');
-    else
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2 from region');
-    end
+    RSK.region = doSelect(RSK, 'select * from region');
     RSK.regionCast = doSelect(RSK, 'select * from regionCast');
 else
     return
