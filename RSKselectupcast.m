61aa8e0b17704e4d92e1f8f71a2e1d18834163e4
Rui Zhang <rui.zhang@rbr-global.com>
2018-05-29 10:14:10 -0400

    update region and regionCast table in RSKselectdown/upcast

--- RSKselectupcast.m
+++ RSKselectupcast.m
@@ -1,6 +1,6 @@
 function [RSK, upidx] = RSKselectupcast(RSK)
 
-%RSKselectupcast - Select the data elements with decreasing pressure.
+% RSKselectupcast - Select the data elements with decreasing pressure.
 %
 % Syntax:  [RSKup, upidx] = RSKselectupcast(RSK)
 %
@@ -19,12 +19,11 @@ function [RSK, upidx] = RSKselectupcast(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2017-06-23
+% Last revision: 2018-05-29
+
 
 Pcol = getchannelindex(RSK, 'Pressure');
 ndata = length(RSK.data);
-
-
 
 upidx = NaN(1, ndata);
 for ndx = 1:ndata
@@ -32,17 +31,17 @@ for ndx = 1:ndata
     upidx(1, ndx) = getcastdirection(pressure, 'up');
 end
 
-
-
 if ~any(upidx ==1)
     disp('No upcasts in this RSK structure.');
     return;
 end
 
-
-
 RSK.profiles.originalindex = RSK.profiles.originalindex(logical(upidx));
 RSK.profiles.order = {'up'};
 RSK.data = RSK.data(logical(upidx));
+
+RSK.region(strncmp({RSK.region.label},'Down',4)) = [];
+RSK.regionCast(strncmp({RSK.regionCast.type},'Down',4)) = [];
+
 
 end
