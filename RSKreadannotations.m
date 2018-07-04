0f163969a2e6ca97a523dedbfb37840eecb4b080
Rui Zhang <rui.zhang@rbr-global.com>
2018-07-04 17:00:01 -0400

    check existance of region table RSKTOOLS-173

--- RSKreadannotations.m
+++ RSKreadannotations.m
@@ -19,17 +19,11 @@ function RSK = RSKreadannotations(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2018-02-22
+% Last revision: 2018-07-04
 
 tables = doSelect(RSK, 'SELECT name FROM sqlite_master WHERE type="table"');
 
 if any(strcmpi({tables.name}, 'region')) && any(strcmpi({tables.name}, 'regionGeoData')) && any(strcmpi({tables.name}, 'regionComment'))
-    regioninfo = doSelect(RSK, 'PRAGMA table_info(region)');
-    if any(strcmpi({regioninfo.name}, 'description')) % description column only exists after 1.13.8 
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2, description from region');
-    else
-        RSK.region = doSelect(RSK, 'select regionID, type, tstamp1/1.0 as tstamp1, tstamp2/1.0 as tstamp2 from region');
-    end
     RSK.regionGeoData = doSelect(RSK, 'select * from regionGeoData');
     RSK.regionComment = doSelect(RSK, 'select * from regionComment');
 else
@@ -40,20 +34,22 @@ if ~isempty(RSK.regionGeoData) && isfield(RSK,'geodata');
     RSK = rmfield(RSK, 'geodata'); % delete cell gps if annotation gps exists
 end
 
-if any(cellfun(@isempty,{RSK.region.description}))
-    RSK.region = rmfield(RSK.region, 'description');
-end
+if isempty(RSK.regionGeoData), RSK = rmfield(RSK,'regionGeoData'); end
+if isempty(RSK.regionComment), RSK = rmfield(RSK,'regionComment'); end
 
-ProfileRegionID = find(strcmpi({RSK.region.type},'PROFILE') == 1);
-GPSRegionID = find(strcmpi({RSK.region.type},'GPS') == 1);
-CommentRegionID = find(strcmpi({RSK.region.type},'COMMENT') == 1);
+if isfield(RSK,'region')
 
-GPSAssignID = zeros(length(GPSRegionID),1);
-CommentAssignID = zeros(length(CommentRegionID),1);
+    if any(cellfun(@isempty,{RSK.region.description}))
+        RSK.region = rmfield(RSK.region, 'description');
+    end
 
-if isempty(RSK.regionGeoData)
-    RSK = rmfield(RSK, 'regionGeoData');
-else
+    ProfileRegionID = find(strcmpi({RSK.region.type},'PROFILE') == 1);
+    GPSRegionID = find(strcmpi({RSK.region.type},'GPS') == 1);
+    CommentRegionID = find(strcmpi({RSK.region.type},'COMMENT') == 1);
+
+    GPSAssignID = zeros(length(GPSRegionID),1);
+    CommentAssignID = zeros(length(CommentRegionID),1);
+
     for g = 1:length(GPSRegionID)
         for p = 1:length(ProfileRegionID)
             if RSK.region(GPSRegionID(g)).tstamp1 >= RSK.region(ProfileRegionID(p)).tstamp1 && ...
@@ -73,11 +69,7 @@ else
             RSK.profiles.GPS.longitude(ndx,1) = nan;
         end
     end
-end
 
-if isempty(RSK.regionComment)
-    RSK = rmfield(RSK, 'regionComment');
-else
     for g = 1:length(CommentRegionID)
         for p = 1:length(ProfileRegionID)
             if RSK.region(CommentRegionID(g)).tstamp1 >= RSK.region(ProfileRegionID(p)).tstamp1 && ...
@@ -95,7 +87,4 @@ else
             RSK.profiles.comment{ndx,1} = nan;
         end
     end
-end
-
-
 end
