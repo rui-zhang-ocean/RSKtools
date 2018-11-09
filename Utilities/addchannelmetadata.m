9b712984b02118d0a579d20486b6cee15270ed82
Rui Zhang <rui.zhang@rbr-global.com>
2018-11-09 15:26:24 -0500

    do not change instrumentChannels when deriving new channels, keep it an original copy from rsk file addchannelmetadata

--- Utilities/addchannelmetadata.m
+++ Utilities/addchannelmetadata.m
@@ -4,8 +4,8 @@ function RSK = addchannelmetadata(RSK, shortName, longName, units)
 %
 % Syntax:  [RSK] = ADDCHANNELMETADATA(RSK, shortName, longName, units)
 % 
-% Adds all the metadata associated with a new channel in the fields,
-% channels and instrumentsChannels, of the RSK structure.
+% Adds all the metadata associated with a new channel in the channel field
+% of the RSK structure.
 %
 % Inputs:
 %   RSK - Input RSK structure
@@ -24,7 +24,7 @@ function RSK = addchannelmetadata(RSK, shortName, longName, units)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2018-05-29
+% Last revision: 2018-11-09
 
 hasChan = any(strcmpi({RSK.channels.longName}, longName));
 
@@ -33,21 +33,6 @@ if ~hasChan
     RSK.channels(nchannels+1).shortName = shortName;
     RSK.channels(nchannels+1).longName = longName;
     RSK.channels(nchannels+1).units = units;
-    
-    
-    if isfield(RSK, 'instrumentChannels')
-        if isfield(RSK.instrumentChannels, 'instrumentID')
-            RSK.instrumentChannels(nchannels+1).instrumentID = RSK.instrumentChannels(1).instrumentID;
-        end
-        if isfield(RSK.instrumentChannels, 'channelStatus')
-            RSK.instrumentChannels(nchannels+1).channelStatus = 0;
-        end
-        if isfield(RSK.instrumentChannels, 'serialID')
-            RSK.instrumentChannels(nchannels+1).serialID = RSK.instrumentChannels(1).serialID;
-        end
-        RSK.instrumentChannels(nchannels+1).channelID = RSK.instrumentChannels(nchannels).channelID+1;
-        RSK.instrumentChannels(nchannels+1).channelOrder = RSK.instrumentChannels(nchannels).channelOrder+1;
-    end
 end
 
 end
