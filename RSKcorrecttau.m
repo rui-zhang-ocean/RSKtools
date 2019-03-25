0eb59d7aeda90d1f4e74b93bfeed1869c5285098
Rui Zhang <rui.zhang@rbr-global.com>
2019-03-25 11:55:18 -0400

    RSKcorrecttau added RSKTOOLS-229

---
+++ RSKcorrecttau.m
@@ -0,0 +1,118 @@
+function RSK = RSKcorrecttau(RSK, channel, varargin)
+
+% RSKcorrecttau - Apply tau correction and smoothing (optional) algorithm 
+% from Fozdar et al. (1985)
+%
+% Syntax:  RSK = RSKcorrecttau(RSK, channel, tauResponse, [OPTIONS])
+% 
+% Blablabla..
+%    
+% Inputs:
+%   [Required] - RSK - Structure containing logger data.
+%
+%                channel - Longname of channel to apply tau correction
+%                      (e.g., temperature, salinity, etc).
+%
+%                tauResponse - time constant of specified channel in
+%                      seconds.
+%
+%   [Optional] - tauSmooth - smoothing time scale in seconds. Default is 0.
+%
+%                profile - Profile number. Default is all available 
+%                      profiles.
+%
+%                direction - 'up' for upcast, 'down' for downcast, or
+%                      'both' for all. Default is all directions available.
+%
+%                visualize - To give a diagnostic plot on specified profile 
+%                      number(s). Original and processed data will be 
+%                      plotted to show users how the algorithm works. 
+%                      Default is 0.
+%
+% Outputs:
+%    RSK - Structure with tau correction on specified channel.
+%
+% Example: 
+%    rsk = RSKcorrecttau(rsk,'Dissolved O2',1)
+%    OR
+%    rsk = RSKcorrecttau(rsk,'Temperature',1,'tauSmooth',0.2,'direction','down','profile',1); 
+%
+% See also: RSKremoveloops, RSKsmooth.
+%
+% Author: RBR Ltd. Ottawa ON, Canada
+% email: support@rbr-global.com
+% Website: www.rbr-global.com
+% Last revision: 2019-03-22
+
+
+validDirections = {'down', 'up', 'both'};
+checkDirection = @(x) any(validatestring(x,validDirections));
+
+p = inputParser;
+addRequired(p, 'RSK', @isstruct);
+addRequired(p, 'channel', @ischar);
+addRequired(p, 'tauResponse', @isnumeric);
+addParameter(p, 'tauSmooth', 0, @isnumeric);
+addParameter(p, 'profile', [], @isnumeric);
+addParameter(p, 'direction', [], checkDirection);
+addParameter(p, 'visualize', 0, @isnumeric);
+parse(p, RSK, channel, varargin{:})
+
+RSK = p.Results.RSK;
+channel = p.Results.channel;
+tauResponse = p.Results.tauResponse;
+tauSmooth = p.Results.tauSmooth;
+profile = p.Results.profile;
+direction = p.Results.direction;
+visualize = p.Results.visualize;
+
+
+channelCol = getchannelindex(RSK, channel);
+castidx = getdataindex(RSK, profile, direction);
+
+if visualize ~= 0; 
+    [raw, diagndx] = checkDiagPlot(RSK, visualize, direction, castidx); 
+end
+
+dt = readsamplingperiod(RSK);
+ar = exp(-dt/tauResponse);
+as = exp(-dt/tauSmooth);
+
+for ndx = castidx
+    in = RSK.data(ndx).values(:,channelCol);   
+    intime = RSK.data(ndx).tstamp;
+    out = correcttau(in, intime, ar, as);   
+    RSK.data(ndx).values(:,channelCol) = out;
+    if visualize ~= 0      
+        for d = diagndx;
+            if ndx == d;
+                figure
+                doDiagPlot(RSK,raw,'ndx',ndx,'channelidx',channelCol,'fn',mfilename); 
+            end
+        end
+    end 
+end
+
+
+logdata = logentrydata(RSK, profile, direction);
+logentry = sprintf('%s tau-corrected using tau response time of %1.0f sec and tau smooth time of %1.0f sec on %s. '...
+           , channel, tauResponse, tauSmooth, logdata);
+RSK = RSKappendtolog(RSK, logentry);
+
+
+%% Nested Functions
+function out = correcttau(in, intime, ar, as)
+% blablabla...  
+
+    ind = isfinite(in);
+    in_itp = interp1(intime(ind),in(ind),intime,'linear','extrap');    
+    out = NaN(size(in));
+    out(1) = in(1);
+
+    for j = 2:length(out)
+        out(j) = ((1-as)./(1-ar)).*(in_itp(j) - ar*in_itp(j-1)) + as*out(j-1);   
+    end
+    out(~ind) = NaN; 
+end
+
+end
