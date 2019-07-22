81fb0f4ee616f40c1d839bd273b414f9866c8420
Rui Zhang <rui.zhang@rbr-global.com>
2019-07-22 14:52:51 -0400

    vectorize latitude/longitude inputs and finalize header text

--- RSKderivesigma.m
+++ RSKderivesigma.m
@@ -5,8 +5,26 @@ function [RSK] = RSKderivesigma(RSK, varargin)
 % Syntax: [RSK] = RSKderivesigma(RSK, [OPTIONS])
 % 
 % Derives potential density anomaly using the TEOS-10 GSW toolbox
-% (http://www.teos-10.org/software.htm). The result is added to the
-% RSK data structure, and the channel list is updated. 
+% (http://www.teos-10.org/software.htm). The result is added to the RSK 
+% data structure, and the channel list is updated. The workflow of the
+% function is as below:
+%
+% 1, Calculate absolute salinity (SA)
+%    a) When latitude and longitude data are available (either from
+%    optional input or station data in RSK.data.latitude/longitude), the
+%    function will call SA = gsw_SA_from_SP(salinity,seapressure,lon,lat)
+%    b) When latitude and longitude data are absent, the function will call
+%    SA = gsw_SR_from_SP(salinity) assuming that reference salinity equals
+%    absolute salinity approximately.
+% 2, Calculate potential temperature (pt0)
+%    pt0 = gsw_pt0_from_t(absolute salinity,temperature,seapressure)
+% 3, Calculate potential density anomaly (sigma0)
+%    sigma0 = gsw_sigma0_pt0_exact(absolute salinity,potential temperature)
+%
+% Note: When geographic information are both available from optional inputs
+% and RSK.data structure, the optional inputs will override. The inputs
+% latitude/longitude must be either a single value of vector of the same
+% length of RSK.data.
 %
 % Inputs: 
 %   [Required] - RSK - Structure containing the logger metadata and data
@@ -23,7 +41,7 @@ function [RSK] = RSKderivesigma(RSK, varargin)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2019-07-18
+% Last revision: 2019-07-22
 
 
 p = inputParser;
@@ -42,6 +60,14 @@ if ~hasTEOS
     error('Must install TEOS-10 toolbox. Download it from here: http://www.teos-10.org/software.htm');
 end
 
+if length(latitude) > 1 && length(RSK.data) ~= length(latitude)
+    error('Input latitude must be either one value or vector of the same length of RSK.data')
+end
+
+if length(longitude) > 1 && length(RSK.data) ~= length(longitude)
+    error('Input longitude must be either one value or vector of the same length of RSK.data')
+end
+
 [Tcol,Scol,SPcol] = getchannel_T_S_SP_index(RSK);
 
 RSK = addchannelmetadata(RSK, 'dden00', 'Density Anomaly', 'kg/m³');
@@ -52,7 +78,8 @@ for ndx = castidx
     SP = RSK.data(ndx).values(:,SPcol);
     S = RSK.data(ndx).values(:,Scol);
     T = RSK.data(ndx).values(:,Tcol);   
-    DA = derive_DA(S,T,SP,latitude,longitude);    
+    [lat,lon] = getGeo(RSK,ndx,latitude,longitude);
+    DA = derive_DA(S,T,SP,lat,lon);    
     RSK.data(ndx).values(:,DAcol) = DA;
 end
 
@@ -75,11 +102,29 @@ function [Tcol,Scol,SPcol] = getchannel_T_S_SP_index(RSK)
     end
 end
 
-function DA = derive_DA(S,T,SP,latitude,longitude)
-    if isempty(latitude) || isempty(longitude)
+function  [lat,lon] = getGeo(RSK,ndx,latitude,longitude)
+    if ~isempty(latitude) && length(latitude) > 1 
+        lat = latitude(ndx);  
+    elseif isempty(latitude) && isfield(RSK.data,'latitude')
+        lat = RSK.data(ndx).latitude; 
+    else
+        lat = latitude;    
+    end
+    
+    if ~isempty(longitude) && length(longitude) > 1 
+        lon = longitude(ndx);  
+    elseif isempty(longitude) && isfield(RSK.data,'longitude')
+        lon = RSK.data(ndx).longitude; 
+    else
+        lon = longitude;    
+    end
+end
+
+function DA = derive_DA(S,T,SP,lat,lon)
+    if isempty(lat) || isempty(lon)
         SA = gsw_SR_from_SP(S); % Assume SA ~= SR
     else
-        SA = gsw_SA_from_SP(S,SP,longitude,latitude);
+        SA = gsw_SA_from_SP(S,SP,lon,lat);
     end
     pt0 = gsw_pt0_from_t(SA,T,SP);
     DA = gsw_sigma0_pt0_exact(SA,pt0);
