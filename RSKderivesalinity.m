dc9327547efeeeb1c19300c430e72168e27600d9
Rui Zhang <rui.zhang@rbr-global.com>
2019-11-12 14:44:44 -0500

    allow RSKderivesalinity to use seawater toolbox when TEOS-10 is not available

--- RSKderivesalinity.m
+++ RSKderivesalinity.m
@@ -5,10 +5,11 @@ function [RSK] = RSKderivesalinity(RSK)
 % Syntax:  [RSK] = RSKderivesalinty(RSK)
 % 
 % Derives salinity using the TEOS-10 GSW toolbox
-% (http://www.teos-10.org/software.htm). The result is added to the
-% RSK data structure, and the channel list is updated. If salinity is
-% already in the RSK data structure (i.e., from Ruskin), it will be
-% overwritten by RSKderivesalinity.
+% (http://www.teos-10.org/software.htm) or sea water toolbox 
+% (http://www.cmar.csiro.au/datacentre/ext_docs/seawater.htm). 
+% The result is added to the RSK data structure, and the channel 
+% list is updated. If salinity is already in the RSK data 
+% structure (i.e., from Ruskin), it will be overwritten.
 %
 % Inputs: 
 %    RSK - Structure containing the logger metadata and data.         
@@ -21,36 +22,32 @@ function [RSK] = RSKderivesalinity(RSK)
 % Author: RBR Ltd. Ottawa ON, Canada
 % email: support@rbr-global.com
 % Website: www.rbr-global.com
-% Last revision: 2018-05-29
+% Last revision: 2019-11-12
+
 
 hasTEOS = ~isempty(which('gsw_SP_from_C'));
-if ~hasTEOS
-    error('Must install TEOS-10 toolbox. Download it from here: http://www.teos-10.org/software.htm');
+hasSW = ~isempty(which('sw_salt'));
+
+if ~hasTEOS && ~hasSW
+    error('Must install TEOS-10 or seawater toolbox. Download it from here: http://www.teos-10.org/software.htm');
 end
     
-
-
-Ccol = getchannelindex(RSK, 'Conductivity');
-Tcol = getchannelindex(RSK, 'Temperature');
-
-
-
 RSK = addchannelmetadata(RSK, 'sal_00', 'Salinity', 'PSU');
-Scol = getchannelindex(RSK, 'Salinity');
+[Ccol,Tcol,Scol] = getchannelindex(RSK,{'Conductivity','Temperature','Salinity'});
 [RSKsp, SPcol] = getseapressure(RSK);
-
-
 
 castidx = getdataindex(RSK);
 for ndx = castidx
-    salinity = gsw_SP_from_C(RSK.data(ndx).values(:, Ccol), RSK.data(ndx).values(:, Tcol), RSKsp.data(ndx).values(:,SPcol));
+    if hasTEOS
+        salinity = gsw_SP_from_C(RSK.data(ndx).values(:, Ccol), RSK.data(ndx).values(:, Tcol), RSKsp.data(ndx).values(:,SPcol));
+        logentry = ('Practical Salinity derived using TEOS-10 GSW toolbox.');
+    else
+        salinity = sw_salt(RSK.data(ndx).values(:, Ccol)/sw_c3515, RSK.data(ndx).values(:, Tcol), RSKsp.data(ndx).values(:,SPcol));
+        logentry = ('Practical Salinity derived using seawater toolbox.');
+    end
     RSK.data(ndx).values(:,Scol) = salinity;
 end
 
-logentry = ('Practical Salinity derived using TEOS-10 GSW toolbox.');
 RSK = RSKappendtolog(RSK, logentry);
 
 end
-
-
-
