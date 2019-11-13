cf7ad0258e3104af846e8e557504c42e532c3671
Rui Zhang <rui.zhang@rbr-global.com>
2019-11-13 16:11:33 -0500

    finalize error message RSKderivesalinity

--- RSKderivesalinity.m
+++ RSKderivesalinity.m
@@ -52,7 +52,7 @@ hasSW = ~isempty(which('sw_salt'));
 if ~hasTEOS && ~hasSW
     error('Must install TEOS-10 (recommended, download it from http://www.teos-10.org/software.htm) or seawater toolbox.');
 elseif ~hasTEOS && strcmpi(toolbox,'TEOS-10')
-    error('No TEOS-10 toolbox found on your MATLAB pathway.')
+    error('No TEOS-10 toolbox found on your MATLAB pathway. Please download it from http://www.teos-10.org/software.htm or specify seawater toolbox.')
 elseif ~hasSW && strcmpi(toolbox,'seawater')
     error('No seawater toolbox found on your MATLAB pathway.')
 else
