c49cb921e82d695f35a304b0a4ba64dd43101673
Rui Zhang <rui.zhang@rbr-global.com>
2020-01-17 14:38:12 -0500

    modify functions have parameters overlapped with RSKsettings, delete private/loadconstants

--- RSKtimeseries2profiles.m
+++ RSKtimeseries2profiles.m
@@ -44,10 +44,12 @@ function RSK = RSKtimeseries2profiles(RSK, varargin)
 % Last revision: 2018-10-30
 
 
+rsksettings = RSKsettings;
+
 p = inputParser;
 addRequired(p, 'RSK', @isstruct);
-addParameter(p, 'pressureThreshold', 3, @isnumeric);
-addParameter(p, 'conductivityThreshold', 0.05, @isnumeric);
+addParameter(p, 'pressureThreshold', rsksettings.pressureThreshold, @isnumeric);
+addParameter(p, 'conductivityThreshold', rsksettings.conductivityThreshold, @isnumeric);
 parse(p, RSK, varargin{:})
 
 RSK = p.Results.RSK;
--- private/loadconstants.m
+++
@@ -1,8 +0,0 @@
-% loadconstants - A collection of constants to be used in RSKtools
-%
-% Author: RBR Ltd. Ottawa ON, Canada
-% email: support@rbr-global.com
-% Website: www.rbr-global.com
-% Last revision: 2019-11-15
-
-defaultpAtm = 10.1325;
